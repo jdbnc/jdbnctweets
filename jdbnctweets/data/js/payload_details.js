34e66fcf635117373027c49056644a4dfc476af6
John Bence <johndbence@gmail.com>
2016-11-03 23:19:34 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2739,
-	"created_at": "Thu Nov 03 03:19:34 +0000 2016",
+	"tweets": 2740,
+	"created_at": "Fri Nov 04 03:19:34 +0000 2016",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2016_11.js
+++ jdbnctweets/data/js/tweets/2016_11.js
@@ -1,6 +1,223 @@
 Grailbird.data.tweets_2016_11 = 
 [
 	{
+		"created_at": "Thu Nov 03 15:28:59 +0000 2016",
+		"id": 794199704737370100,
+		"id_str": "794199704737370112",
+		"text": "I was there! https://t.co/SjyuzsPes5",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SjyuzsPes5",
+					"expanded_url": "https://twitter.com/KenyonCollege/status/794194495956992000",
+					"display_url": "twitter.com/KenyonCollege/…",
+					"indices": [
+						13,
+						36
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta ",
+			"description": "University Archivist at @EmoryUniversity in @EmoryRoseMARBL, tweeting abt archives, libraries, history, Atlanta, GA + some inane prattlings. Tweets are my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 550,
+			"friends_count": 676,
+			"listed_count": 27,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 913,
+			"utc_offset": -14400,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2735,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1473125136",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 794194495956992000,
+		"quoted_status_id_str": "794194495956992000",
+		"quoted_status": {
+			"created_at": "Thu Nov 03 15:08:17 +0000 2016",
+			"id": 794194495956992000,
+			"id_str": "794194495956992000",
+			"text": "#TBT to Nov. 2, 2004 when #KenyonCollege students waited upwards of 11 hours to vote. From @KenyonCollegian:… https://t.co/5egvVR003C",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "TBT",
+						"indices": [
+							0,
+							4
+						]
+					},
+					{
+						"text": "KenyonCollege",
+						"indices": [
+							26,
+							40
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "KenyonCollegian",
+						"name": "The Kenyon Collegian",
+						"id": 243800609,
+						"id_str": "243800609",
+						"indices": [
+							91,
+							107
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/5egvVR003C",
+						"expanded_url": "https://twitter.com/i/web/status/794194495956992000",
+						"display_url": "twitter.com/i/web/status/7…",
+						"indices": [
+							110,
+							133
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 29245121,
+				"id_str": "29245121",
+				"name": "Kenyon College",
+				"screen_name": "KenyonCollege",
+				"location": "Gambier, Ohio",
+				"description": "Founded in 1824, #KenyonCollege is a top liberal arts college with 1,650 students. Snapchat: kenyoncollege",
+				"url": "https://t.co/G5uUKP9Sb3",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/G5uUKP9Sb3",
+								"expanded_url": "http://www.kenyon.edu",
+								"display_url": "kenyon.edu",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 8207,
+				"friends_count": 826,
+				"listed_count": 184,
+				"created_at": "Mon Apr 06 17:19:06 +0000 2009",
+				"favourites_count": 1928,
+				"utc_offset": -14400,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 5316,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "59118E",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/536954724906328064/J3-bI2J-.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/536954724906328064/J3-bI2J-.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/768850932570460160/mQDPgQV9_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/768850932570460160/mQDPgQV9_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/29245121/1472237321",
+				"profile_link_color": "003399",
+				"profile_sidebar_border_color": "59118E",
+				"profile_sidebar_fill_color": "FFF7D6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 14,
+			"favorite_count": 18,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Nov 02 21:16:36 +0000 2016",
 		"id": 793924796773982200,
 		"id_str": "793924796773982209",
