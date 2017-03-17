570c564cc46863b715581a3d05dbd7ceb3ae81f2
John Bence <johndbence@gmail.com>
2017-03-16 23:19:35 -0400

    Added 2 tweets

--- jdbnctweets/data/js/tweets/2017_03.js
+++ jdbnctweets/data/js/tweets/2017_03.js
@@ -1,6 +1,565 @@
 Grailbird.data.tweets_2017_03 = 
 [
 	{
+		"created_at": "Thu Mar 16 23:23:36 +0000 2017",
+		"id": 842516731029610500,
+		"id_str": "842516731029610497",
+		"text": "I helped do a thing! https://t.co/jm8eTPqBrd",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jm8eTPqBrd",
+					"expanded_url": "https://twitter.com/EmoryUniversity/status/842496069837688838",
+					"display_url": "twitter.com/EmoryUniversit…",
+					"indices": [
+						21,
+						44
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta ",
+			"description": "University Archivist for @EmoryUniversity in @EmoryRoseMARBL, tweeting abt archives, libraries, history, Atlanta, GA + some inane prattlings. Tweets are my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 579,
+			"friends_count": 717,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 1135,
+			"utc_offset": -14400,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2953,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1485439135",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 842496069837688800,
+		"quoted_status_id_str": "842496069837688838",
+		"quoted_status": {
+			"created_at": "Thu Mar 16 22:01:30 +0000 2017",
+			"id": 842496069837688800,
+			"id_str": "842496069837688838",
+			"text": "A century after Eléonore Raoul was admitted to @EmoryLaw, #Emory celebrates 100 years of women at Emory… https://t.co/3nxL0UwvXm",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Emory",
+						"indices": [
+							58,
+							64
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "EmoryLaw",
+						"name": "Emory Law",
+						"id": 16032593,
+						"id_str": "16032593",
+						"indices": [
+							47,
+							56
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/3nxL0UwvXm",
+						"expanded_url": "https://twitter.com/i/web/status/842496069837688838",
+						"display_url": "twitter.com/i/web/status/8…",
+						"indices": [
+							105,
+							128
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16438655,
+				"id_str": "16438655",
+				"name": "Emory University",
+				"screen_name": "EmoryUniversity",
+				"location": "Atlanta, GA",
+				"description": "Official twitter account for Emory, a private research university of international reach where courageous ideas achieve positive transformation in the world.",
+				"url": "http://t.co/FldXSUaaxt",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/FldXSUaaxt",
+								"expanded_url": "http://www.emory.edu",
+								"display_url": "emory.edu",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 33975,
+				"friends_count": 581,
+				"listed_count": 929,
+				"created_at": "Wed Sep 24 18:59:09 +0000 2008",
+				"favourites_count": 7337,
+				"utc_offset": -14400,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 8300,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000066",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/469569964332965888/PJ4uYUjz.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/469569964332965888/PJ4uYUjz.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/469598559780605953/xodpcszf_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/469598559780605953/xodpcszf_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16438655/1487966643",
+				"profile_link_color": "000066",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "CC9900",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7,
+			"favorite_count": 10,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Thu Mar 16 13:30:15 +0000 2017",
+		"id": 842367408228692000,
+		"id_str": "842367408228691970",
+		"text": "RT @StateArchivists: #VT #statearchives helping journalists protect sources and do their jobs https://t.co/ANzSz70ou6",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "VT",
+					"indices": [
+						21,
+						24
+					]
+				},
+				{
+					"text": "statearchives",
+					"indices": [
+						25,
+						39
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StateArchivists",
+					"name": "CoSA",
+					"id": 2411453256,
+					"id_str": "2411453256",
+					"indices": [
+						3,
+						19
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ANzSz70ou6",
+					"expanded_url": "https://twitter.com/VermontSOS/status/842355885624197122",
+					"display_url": "twitter.com/VermontSOS/sta…",
+					"indices": [
+						94,
+						117
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta ",
+			"description": "University Archivist for @EmoryUniversity in @EmoryRoseMARBL, tweeting abt archives, libraries, history, Atlanta, GA + some inane prattlings. Tweets are my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 579,
+			"friends_count": 717,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 1135,
+			"utc_offset": -14400,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2953,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1485439135",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Mar 16 13:28:45 +0000 2017",
+			"id": 842367033174044700,
+			"id_str": "842367033174044672",
+			"text": "#VT #statearchives helping journalists protect sources and do their jobs https://t.co/ANzSz70ou6",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "VT",
+						"indices": [
+							0,
+							3
+						]
+					},
+					{
+						"text": "statearchives",
+						"indices": [
+							4,
+							18
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/ANzSz70ou6",
+						"expanded_url": "https://twitter.com/VermontSOS/status/842355885624197122",
+						"display_url": "twitter.com/VermontSOS/sta…",
+						"indices": [
+							73,
+							96
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2411453256,
+				"id_str": "2411453256",
+				"name": "CoSA",
+				"screen_name": "StateArchivists",
+				"location": "",
+				"description": "A national nonprofit organization facilitating networking and project collaboration among the directors and staff of state and territorial archival agencies.",
+				"url": "http://t.co/sgiTHAvSrw",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/sgiTHAvSrw",
+								"expanded_url": "http://www.statearchivists.org",
+								"display_url": "statearchivists.org",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 714,
+				"friends_count": 276,
+				"listed_count": 65,
+				"created_at": "Tue Mar 25 19:46:06 +0000 2014",
+				"favourites_count": 33,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 2733,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/448869060575387648/Q8wfBUL3_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/448869060575387648/Q8wfBUL3_normal.jpeg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 842355885624197100,
+			"quoted_status_id_str": "842355885624197122",
+			"quoted_status": {
+				"created_at": "Thu Mar 16 12:44:28 +0000 2017",
+				"id": 842355885624197100,
+				"id_str": "842355885624197122",
+				"text": "Relevant for #sunshineweek: Why Vermont needs a shield law for journalists https://t.co/xJUHv92rCD #vtpoli",
+				"truncated": false,
+				"entities": {
+					"hashtags": [
+						{
+							"text": "sunshineweek",
+							"indices": [
+								13,
+								26
+							]
+						},
+						{
+							"text": "vtpoli",
+							"indices": [
+								99,
+								106
+							]
+						}
+					],
+					"symbols": [],
+					"user_mentions": [],
+					"urls": [
+						{
+							"url": "https://t.co/xJUHv92rCD",
+							"expanded_url": "https://vtdigger.org/2017/03/14/dave-gram-vermont-needs-shield-law-journalists/",
+							"display_url": "vtdigger.org/2017/03/14/dav…",
+							"indices": [
+								75,
+								98
+							]
+						}
+					]
+				},
+				"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+				"in_reply_to_status_id": null,
+				"in_reply_to_status_id_str": null,
+				"in_reply_to_user_id": null,
+				"in_reply_to_user_id_str": null,
+				"in_reply_to_screen_name": null,
+				"user": {
+					"id": 3946773623,
+					"id_str": "3946773623",
+					"name": "VT Sec. of State",
+					"screen_name": "VermontSOS",
+					"location": "Vermont, USA",
+					"description": "News & updates from Sec. of State Jim Condos’ office about elections, professional regulation, business registration & state archives. Tweets managed by staff.",
+					"url": "http://t.co/QhgivutGK8",
+					"entities": {
+						"url": {
+							"urls": [
+								{
+									"url": "http://t.co/QhgivutGK8",
+									"expanded_url": "http://www.sec.state.vt.us",
+									"display_url": "sec.state.vt.us",
+									"indices": [
+										0,
+										22
+									]
+								}
+							]
+						},
+						"description": {
+							"urls": []
+						}
+					},
+					"protected": false,
+					"followers_count": 1044,
+					"friends_count": 271,
+					"listed_count": 43,
+					"created_at": "Tue Oct 13 13:25:15 +0000 2015",
+					"favourites_count": 882,
+					"utc_offset": null,
+					"time_zone": null,
+					"geo_enabled": false,
+					"verified": false,
+					"statuses_count": 1411,
+					"lang": "en",
+					"contributors_enabled": false,
+					"is_translator": false,
+					"is_translation_enabled": false,
+					"profile_background_color": "000000",
+					"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+					"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+					"profile_background_tile": false,
+					"profile_image_url": "http://pbs.twimg.com/profile_images/654283561332252672/I4-ODwz2_normal.png",
+					"profile_image_url_https": "https://pbs.twimg.com/profile_images/654283561332252672/I4-ODwz2_normal.png",
+					"profile_banner_url": "https://pbs.twimg.com/profile_banners/3946773623/1444828479",
+					"profile_link_color": "4A913C",
+					"profile_sidebar_border_color": "000000",
+					"profile_sidebar_fill_color": "000000",
+					"profile_text_color": "000000",
+					"profile_use_background_image": false,
+					"has_extended_profile": false,
+					"default_profile": false,
+					"default_profile_image": false,
+					"following": false,
+					"follow_request_sent": false,
+					"notifications": false,
+					"translator_type": "none"
+				},
+				"geo": null,
+				"coordinates": null,
+				"place": null,
+				"contributors": null,
+				"is_quote_status": false,
+				"retweet_count": 3,
+				"favorite_count": 2,
+				"favorited": false,
+				"retweeted": false,
+				"possibly_sensitive": false,
+				"lang": "en"
+			},
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": true,
+		"quoted_status_id": 842355885624197100,
+		"quoted_status_id_str": "842355885624197122",
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Mar 15 20:31:30 +0000 2017",
 		"id": 842111033502253000,
 		"id_str": "842111033502253056",
