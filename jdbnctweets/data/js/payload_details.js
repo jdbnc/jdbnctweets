8255b2d823159182233ab4c2784f3b5affc67305
John Bence <johndbence@gmail.com>
2017-04-13 23:19:35 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3004,
-	"created_at": "Thu Apr 13 03:19:35 +0000 2017",
+	"tweets": 3005,
+	"created_at": "Fri Apr 14 03:19:34 +0000 2017",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2017_04.js
+++ jdbnctweets/data/js/tweets/2017_04.js
@@ -1,6 +1,401 @@
 Grailbird.data.tweets_2017_04 = 
 [
 	{
+		"created_at": "Thu Apr 13 14:30:16 +0000 2017",
+		"id": 852529371722264600,
+		"id_str": "852529371722264578",
+		"text": "RT @EmoryRoseMARBL: #TBT to when #Emory student dances were far out, man. @EmoryUniversity https://t.co/Kso0tMNaAh",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TBT",
+					"indices": [
+						20,
+						24
+					]
+				},
+				{
+					"text": "Emory",
+					"indices": [
+						33,
+						39
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EmoryRoseMARBL",
+					"name": "Rose Library Emory",
+					"id": 2979785051,
+					"id_str": "2979785051",
+					"indices": [
+						3,
+						18
+					]
+				},
+				{
+					"screen_name": "EmoryUniversity",
+					"name": "Emory University",
+					"id": 16438655,
+					"id_str": "16438655",
+					"indices": [
+						74,
+						90
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 852528214274691100,
+					"id_str": "852528214274691073",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/media/C9TJ5NzXUAEYS9D.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C9TJ5NzXUAEYS9D.jpg",
+					"url": "https://t.co/Kso0tMNaAh",
+					"display_url": "pic.twitter.com/Kso0tMNaAh",
+					"expanded_url": "https://twitter.com/EmoryRoseMARBL/status/852528413751607296/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 468,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1410,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 826,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 852528413751607300,
+					"source_status_id_str": "852528413751607296",
+					"source_user_id": 2979785051,
+					"source_user_id_str": "2979785051"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 852528214274691100,
+					"id_str": "852528214274691073",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/media/C9TJ5NzXUAEYS9D.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C9TJ5NzXUAEYS9D.jpg",
+					"url": "https://t.co/Kso0tMNaAh",
+					"display_url": "pic.twitter.com/Kso0tMNaAh",
+					"expanded_url": "https://twitter.com/EmoryRoseMARBL/status/852528413751607296/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 468,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1410,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 826,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 852528413751607300,
+					"source_status_id_str": "852528413751607296",
+					"source_user_id": 2979785051,
+					"source_user_id_str": "2979785051"
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta ",
+			"description": "University Archivist for @EmoryUniversity in @EmoryRoseMARBL, tweeting abt archives, libraries, history, Atlanta, GA + some inane prattlings. Tweets are my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 586,
+			"friends_count": 723,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 1168,
+			"utc_offset": -14400,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2986,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1485439135",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Thu Apr 13 14:26:27 +0000 2017",
+			"id": 852528413751607300,
+			"id_str": "852528413751607296",
+			"text": "#TBT to when #Emory student dances were far out, man. @EmoryUniversity https://t.co/Kso0tMNaAh",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "TBT",
+						"indices": [
+							0,
+							4
+						]
+					},
+					{
+						"text": "Emory",
+						"indices": [
+							13,
+							19
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "EmoryUniversity",
+						"name": "Emory University",
+						"id": 16438655,
+						"id_str": "16438655",
+						"indices": [
+							54,
+							70
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 852528214274691100,
+						"id_str": "852528214274691073",
+						"indices": [
+							71,
+							94
+						],
+						"media_url": "http://pbs.twimg.com/media/C9TJ5NzXUAEYS9D.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C9TJ5NzXUAEYS9D.jpg",
+						"url": "https://t.co/Kso0tMNaAh",
+						"display_url": "pic.twitter.com/Kso0tMNaAh",
+						"expanded_url": "https://twitter.com/EmoryRoseMARBL/status/852528413751607296/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 468,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1410,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 826,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 852528214274691100,
+						"id_str": "852528214274691073",
+						"indices": [
+							71,
+							94
+						],
+						"media_url": "http://pbs.twimg.com/media/C9TJ5NzXUAEYS9D.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C9TJ5NzXUAEYS9D.jpg",
+						"url": "https://t.co/Kso0tMNaAh",
+						"display_url": "pic.twitter.com/Kso0tMNaAh",
+						"expanded_url": "https://twitter.com/EmoryRoseMARBL/status/852528413751607296/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 468,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1410,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 826,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2979785051,
+				"id_str": "2979785051",
+				"name": "Rose Library Emory",
+				"screen_name": "EmoryRoseMARBL",
+				"location": "Atlanta, GA",
+				"description": "Stuart A. Rose Manuscript, Archives, and Rare Book Library",
+				"url": "http://t.co/tROo8YcJcB",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/tROo8YcJcB",
+								"expanded_url": "http://rose.library.emory.edu/",
+								"display_url": "rose.library.emory.edu",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 405,
+				"friends_count": 169,
+				"listed_count": 12,
+				"created_at": "Thu Jan 15 13:58:49 +0000 2015",
+				"favourites_count": 97,
+				"utc_offset": -14400,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 626,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/555766102718832640/86Gr1wSI_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/555766102718832640/86Gr1wSI_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2979785051/1442582260",
+				"profile_link_color": "00267F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 13,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Wed Apr 12 16:11:16 +0000 2017",
 		"id": 852192403880595500,
 		"id_str": "852192403880595456",
