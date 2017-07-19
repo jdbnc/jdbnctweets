e2933325acdd4560df6b614d5ffcfa3db86884a7
John Bence <johndbence@gmail.com>
2017-07-18 23:19:35 -0400

    Added 2 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3058,
-	"created_at": "Sun Jul 16 03:19:36 +0000 2017",
+	"tweets": 3060,
+	"created_at": "Wed Jul 19 03:19:34 +0000 2017",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2017_07.js
+++ jdbnctweets/data/js/tweets/2017_07.js
@@ -1,6 +1,370 @@
 Grailbird.data.tweets_2017_07 = 
 [
 	{
+		"created_at": "Tue Jul 18 19:42:39 +0000 2017",
+		"id": 887397221943128000,
+		"id_str": "887397221943128064",
+		"text": "RT @ethanahenderson: Hey @KenyonCollege and @uncsils - Anna Kephart '10 helped rescue archival materials (again)!! https://t.co/oKYFtaLa8U…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ethanahenderson",
+					"name": "ethanahenderson",
+					"id": 18170513,
+					"id_str": "18170513",
+					"indices": [
+						3,
+						19
+					]
+				},
+				{
+					"screen_name": "KenyonCollege",
+					"name": "Kenyon College",
+					"id": 29245121,
+					"id_str": "29245121",
+					"indices": [
+						25,
+						39
+					]
+				},
+				{
+					"screen_name": "uncsils",
+					"name": "UNC SILS",
+					"id": 112829445,
+					"id_str": "112829445",
+					"indices": [
+						44,
+						52
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/oKYFtaLa8U",
+					"expanded_url": "http://news.csmd.edu/all-news/csms-southern-maryland-studies-center-rescues-community-record-including-newspaper-photo-materials-from-1980s-1990s/",
+					"display_url": "news.csmd.edu/all-news/csms-…",
+					"indices": [
+						115,
+						138
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta ",
+			"description": "University Archivist for @EmoryUniversity in @EmoryRoseMARBL, tweeting abt archives, libraries, history, Atlanta, GA + some inane prattlings.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 602,
+			"friends_count": 741,
+			"listed_count": 30,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 1268,
+			"utc_offset": -14400,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3038,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1485439135",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon Jul 17 20:10:23 +0000 2017",
+			"id": 887041812535357400,
+			"id_str": "887041812535357443",
+			"text": "Hey @KenyonCollege and @uncsils - Anna Kephart '10 helped rescue archival materials (again)!! https://t.co/oKYFtaLa8U\n#archivist",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "archivist",
+						"indices": [
+							118,
+							128
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "KenyonCollege",
+						"name": "Kenyon College",
+						"id": 29245121,
+						"id_str": "29245121",
+						"indices": [
+							4,
+							18
+						]
+					},
+					{
+						"screen_name": "uncsils",
+						"name": "UNC SILS",
+						"id": 112829445,
+						"id_str": "112829445",
+						"indices": [
+							23,
+							31
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/oKYFtaLa8U",
+						"expanded_url": "http://news.csmd.edu/all-news/csms-southern-maryland-studies-center-rescues-community-record-including-newspaper-photo-materials-from-1980s-1990s/",
+						"display_url": "news.csmd.edu/all-news/csms-…",
+						"indices": [
+							94,
+							117
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18170513,
+				"id_str": "18170513",
+				"name": "ethanahenderson",
+				"screen_name": "ethanahenderson",
+				"location": "Washington, DC",
+				"description": "Curiosity cabinets of libraries, museums, and all things found inside. Dogs. Marathoner. Classical music. And much much more.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 168,
+				"friends_count": 239,
+				"listed_count": 20,
+				"created_at": "Tue Dec 16 19:44:01 +0000 2008",
+				"favourites_count": 2800,
+				"utc_offset": -14400,
+				"time_zone": "Georgetown",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 2843,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C6E2EE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/2845936491/6102f739f69e04e9c4ceb87a687a5a8d_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/2845936491/6102f739f69e04e9c4ceb87a687a5a8d_normal.jpeg",
+				"profile_link_color": "1F98C7",
+				"profile_sidebar_border_color": "C6E2EE",
+				"profile_sidebar_fill_color": "DAECF4",
+				"profile_text_color": "663B12",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Tue Jul 18 16:13:09 +0000 2017",
+		"id": 887344499625779200,
+		"id_str": "887344499625779200",
+		"text": "@EmoryRoseMARBL @EmoryUnivCtr @EmoryUniversity @CampusLifeEmory And here's a t-shirt lamenting the death of the AMU… https://t.co/qNjmwzF2m7",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EmoryRoseMARBL",
+					"name": "Rose Library Emory",
+					"id": 2979785051,
+					"id_str": "2979785051",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "EmoryUnivCtr",
+					"name": "University Center",
+					"id": 207329327,
+					"id_str": "207329327",
+					"indices": [
+						16,
+						29
+					]
+				},
+				{
+					"screen_name": "EmoryUniversity",
+					"name": "Emory University",
+					"id": 16438655,
+					"id_str": "16438655",
+					"indices": [
+						30,
+						46
+					]
+				},
+				{
+					"screen_name": "CampusLifeEmory",
+					"name": "Emory Campus Life",
+					"id": 339241106,
+					"id_str": "339241106",
+					"indices": [
+						47,
+						63
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qNjmwzF2m7",
+					"expanded_url": "https://twitter.com/i/web/status/887344499625779200",
+					"display_url": "twitter.com/i/web/status/8…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 887312207851249700,
+		"in_reply_to_status_id_str": "887312207851249665",
+		"in_reply_to_user_id": 2979785051,
+		"in_reply_to_user_id_str": "2979785051",
+		"in_reply_to_screen_name": "EmoryRoseMARBL",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta ",
+			"description": "University Archivist for @EmoryUniversity in @EmoryRoseMARBL, tweeting abt archives, libraries, history, Atlanta, GA + some inane prattlings.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 602,
+			"friends_count": 741,
+			"listed_count": 30,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 1268,
+			"utc_offset": -14400,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3038,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/715238199606571008/ax21TW4S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1485439135",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Jul 15 16:24:34 +0000 2017",
 		"id": 886260209945018400,
 		"id_str": "886260209945018368",
