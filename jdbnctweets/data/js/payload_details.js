da9a7eba30375c2f83f6a50d1fcf1a816d734722
John Bence <johndbence@gmail.com>
2017-12-04 22:19:36 -0500

    Added 2 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3308,
-	"created_at": "Mon Dec 04 03:19:35 +0000 2017",
+	"tweets": 3310,
+	"created_at": "Tue Dec 05 03:19:34 +0000 2017",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2017_12.js
+++ jdbnctweets/data/js/tweets/2017_12.js
@@ -1,6 +1,474 @@
 Grailbird.data.tweets_2017_12 = 
 [
 	{
+		"created_at": "Mon Dec 04 21:52:51 +0000 2017",
+		"id": 937801900107366400,
+		"id_str": "937801900107366400",
+		"text": "RT @ajc: #BREAKING City of Atlanta’s expansion to Emory and CDC approved https://t.co/2PP8eEoQ8b https://t.co/Wbtj3rPS0G",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BREAKING",
+					"indices": [
+						9,
+						18
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ajc",
+					"name": "AJC",
+					"id": 4170491,
+					"id_str": "4170491",
+					"indices": [
+						3,
+						7
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/2PP8eEoQ8b",
+					"expanded_url": "http://on-ajc.com/2ihBr0X",
+					"display_url": "on-ajc.com/2ihBr0X",
+					"indices": [
+						73,
+						96
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 937800009487065100,
+					"id_str": "937800009487065088",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/DQO8I9PVoAA8Drh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DQO8I9PVoAA8Drh.jpg",
+					"url": "https://t.co/Wbtj3rPS0G",
+					"display_url": "pic.twitter.com/Wbtj3rPS0G",
+					"expanded_url": "https://twitter.com/ajc/status/937800011785687040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 538,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 538,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 457,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 937800011785687000,
+					"source_status_id_str": "937800011785687040",
+					"source_user_id": 4170491,
+					"source_user_id_str": "4170491"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 937800009487065100,
+					"id_str": "937800009487065088",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/DQO8I9PVoAA8Drh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DQO8I9PVoAA8Drh.jpg",
+					"url": "https://t.co/Wbtj3rPS0G",
+					"display_url": "pic.twitter.com/Wbtj3rPS0G",
+					"expanded_url": "https://twitter.com/ajc/status/937800011785687040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 538,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 538,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 457,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 937800011785687000,
+					"source_status_id_str": "937800011785687040",
+					"source_user_id": 4170491,
+					"source_user_id_str": "4170491"
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta ",
+			"description": "University Archivist in @EmoryRoseMARBL at @EmoryUniversity tweeting about archives, records, libraries, history, Atlanta, and some inanities",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 638,
+			"friends_count": 761,
+			"listed_count": 30,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 1760,
+			"utc_offset": -18000,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3261,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/921681538244280326/_YGHrk3x_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/921681538244280326/_YGHrk3x_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1508601718",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon Dec 04 21:45:20 +0000 2017",
+			"id": 937800011785687000,
+			"id_str": "937800011785687040",
+			"text": "#BREAKING City of Atlanta’s expansion to Emory and CDC approved https://t.co/2PP8eEoQ8b https://t.co/Wbtj3rPS0G",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "BREAKING",
+						"indices": [
+							0,
+							9
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/2PP8eEoQ8b",
+						"expanded_url": "http://on-ajc.com/2ihBr0X",
+						"display_url": "on-ajc.com/2ihBr0X",
+						"indices": [
+							64,
+							87
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 937800009487065100,
+						"id_str": "937800009487065088",
+						"indices": [
+							88,
+							111
+						],
+						"media_url": "http://pbs.twimg.com/media/DQO8I9PVoAA8Drh.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DQO8I9PVoAA8Drh.jpg",
+						"url": "https://t.co/Wbtj3rPS0G",
+						"display_url": "pic.twitter.com/Wbtj3rPS0G",
+						"expanded_url": "https://twitter.com/ajc/status/937800011785687040/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 800,
+								"h": 538,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 800,
+								"h": 538,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 457,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 937800009487065100,
+						"id_str": "937800009487065088",
+						"indices": [
+							88,
+							111
+						],
+						"media_url": "http://pbs.twimg.com/media/DQO8I9PVoAA8Drh.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DQO8I9PVoAA8Drh.jpg",
+						"url": "https://t.co/Wbtj3rPS0G",
+						"display_url": "pic.twitter.com/Wbtj3rPS0G",
+						"expanded_url": "https://twitter.com/ajc/status/937800011785687040/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 800,
+								"h": 538,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 800,
+								"h": 538,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 457,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://www.socialflow.com\" rel=\"nofollow\">SocialFlow</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 4170491,
+				"id_str": "4170491",
+				"name": "AJC",
+				"screen_name": "ajc",
+				"location": "Atlanta, GA",
+				"description": "Atlanta's best source for news. You can reach our customer service team here: @AJCassist",
+				"url": "https://t.co/jvkZJ5B2LG",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/jvkZJ5B2LG",
+								"expanded_url": "http://www.ajc.com",
+								"display_url": "ajc.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 993874,
+				"friends_count": 57575,
+				"listed_count": 4825,
+				"created_at": "Wed Apr 11 12:50:15 +0000 2007",
+				"favourites_count": 377,
+				"utc_offset": -18000,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 198828,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "00539B",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000091127693/6d3a47ce57110dc1a703b0110831c19e.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000091127693/6d3a47ce57110dc1a703b0110831c19e.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/846349663905206273/I2kklg7__normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/846349663905206273/I2kklg7__normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/4170491/1510174706",
+				"profile_link_color": "00539B",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "F0F5ED",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 39,
+			"favorite_count": 23,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 39,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Mon Dec 04 15:19:04 +0000 2017",
+		"id": 937702804470280200,
+		"id_str": "937702804470280192",
+		"text": "@AlcosibaTweets Congrats on the new digs! I'm hoping to be right behind you in that... sometime in 2018!",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AlcosibaTweets",
+					"name": "Christeene Alcosiba",
+					"id": 25442192,
+					"id_str": "25442192",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 937700687785480200,
+		"in_reply_to_status_id_str": "937700687785480192",
+		"in_reply_to_user_id": 25442192,
+		"in_reply_to_user_id_str": "25442192",
+		"in_reply_to_screen_name": "AlcosibaTweets",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta ",
+			"description": "University Archivist in @EmoryRoseMARBL at @EmoryUniversity tweeting about archives, records, libraries, history, Atlanta, and some inanities",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 638,
+			"friends_count": 761,
+			"listed_count": 30,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 1760,
+			"utc_offset": -18000,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3261,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/921681538244280326/_YGHrk3x_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/921681538244280326/_YGHrk3x_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1508601718",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sun Dec 03 21:24:15 +0000 2017",
 		"id": 937432318171668500,
 		"id_str": "937432318171668481",
