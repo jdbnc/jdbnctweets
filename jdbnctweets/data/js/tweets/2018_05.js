468f8d22968e5f8046b0b74597cbf57251990cd7
John Bence <johndbence@gmail.com>
2018-05-02 23:19:36 -0400

    Added 1 tweets

---
+++ jdbnctweets/data/js/tweets/2018_05.js
@@ -0,0 +1,399 @@
+Grailbird.data.tweets_2018_05 = 
+[
+	{
+		"created_at": "Wed May 02 10:51:24 +0000 2018",
+		"id": 991631232940245000,
+		"id_str": "991631232940244992",
+		"text": "RT @SenNanOrrock: Find YOUR poll and vote early for @staceyabrams #GAGov https://t.co/218klJTDXc",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GAGov",
+					"indices": [
+						66,
+						72
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SenNanOrrock",
+					"name": "Nan Orrock",
+					"id": 299751154,
+					"id_str": "299751154",
+					"indices": [
+						3,
+						16
+					]
+				},
+				{
+					"screen_name": "staceyabrams",
+					"name": "Stacey Abrams",
+					"id": 216065430,
+					"id_str": "216065430",
+					"indices": [
+						52,
+						65
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/218klJTDXc",
+					"expanded_url": "https://twitter.com/teamabrams/status/990954450985652224",
+					"display_url": "twitter.com/teamabrams/sta…",
+					"indices": [
+						73,
+						96
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta ",
+			"description": "University Archivist in @EmoryRoseMARBL at @EmoryUniversity tweeting about archives, records, libraries, history, Atlanta, and some inanities",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 635,
+			"friends_count": 775,
+			"listed_count": 30,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 1984,
+			"utc_offset": -14400,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3332,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/485808644819144707/CpVFwJcp.jpeg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/921681538244280326/_YGHrk3x_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/921681538244280326/_YGHrk3x_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1508601718",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed May 02 04:57:37 +0000 2018",
+			"id": 991542202454151200,
+			"id_str": "991542202454151168",
+			"text": "Find YOUR poll and vote early for @staceyabrams #GAGov https://t.co/218klJTDXc",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "GAGov",
+						"indices": [
+							48,
+							54
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "staceyabrams",
+						"name": "Stacey Abrams",
+						"id": 216065430,
+						"id_str": "216065430",
+						"indices": [
+							34,
+							47
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/218klJTDXc",
+						"expanded_url": "https://twitter.com/teamabrams/status/990954450985652224",
+						"display_url": "twitter.com/teamabrams/sta…",
+						"indices": [
+							55,
+							78
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 299751154,
+				"id_str": "299751154",
+				"name": "Nan Orrock",
+				"screen_name": "SenNanOrrock",
+				"location": "Atlanta, GA",
+				"description": "Georgia Senator, Atlanta & Fulton Cty. Proud mom, Pres. of WiLL (Women Legislators' Lobby). Pro-democracy, standing up 4 working folks, govt transparency.",
+				"url": "https://t.co/S9GWWFjLdE",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/S9GWWFjLdE",
+								"expanded_url": "http://www.nanorrock.com",
+								"display_url": "nanorrock.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5689,
+				"friends_count": 1567,
+				"listed_count": 224,
+				"created_at": "Mon May 16 16:33:39 +0000 2011",
+				"favourites_count": 2925,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 5868,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/456895691214688257/ht1YW-cm_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/456895691214688257/ht1YW-cm_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/299751154/1402435844",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "4ec01c9dbc693497",
+				"url": "https://api.twitter.com/1.1/geo/id/4ec01c9dbc693497.json",
+				"place_type": "admin",
+				"name": "Florida",
+				"full_name": "Florida, USA",
+				"country_code": "US",
+				"country": "United States",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-87.634643,
+								24.396308
+							],
+							[
+								-79.974307,
+								24.396308
+							],
+							[
+								-79.974307,
+								31.001056
+							],
+							[
+								-87.634643,
+								31.001056
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 990954450985652200,
+			"quoted_status_id_str": "990954450985652224",
+			"quoted_status": {
+				"created_at": "Mon Apr 30 14:02:06 +0000 2018",
+				"id": 990954450985652200,
+				"id_str": "990954450985652224",
+				"text": "Georgia: Our time is now! Early voting starts TODAY and ends on May 18th. Make a plan to vote @staceyabrams for… https://t.co/2IBNnhI6k0",
+				"truncated": true,
+				"entities": {
+					"hashtags": [],
+					"symbols": [],
+					"user_mentions": [
+						{
+							"screen_name": "staceyabrams",
+							"name": "Stacey Abrams",
+							"id": 216065430,
+							"id_str": "216065430",
+							"indices": [
+								94,
+								107
+							]
+						}
+					],
+					"urls": [
+						{
+							"url": "https://t.co/2IBNnhI6k0",
+							"expanded_url": "https://twitter.com/i/web/status/990954450985652224",
+							"display_url": "twitter.com/i/web/status/9…",
+							"indices": [
+								113,
+								136
+							]
+						}
+					]
+				},
+				"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+				"in_reply_to_status_id": null,
+				"in_reply_to_status_id_str": null,
+				"in_reply_to_user_id": null,
+				"in_reply_to_user_id_str": null,
+				"in_reply_to_screen_name": null,
+				"user": {
+					"id": 879771304316072000,
+					"id_str": "879771304316071938",
+					"name": "Team Abrams",
+					"screen_name": "teamabrams",
+					"location": "Georgia, USA",
+					"description": "The grassroots account for @StaceyAbrams' campaign for Georgia Governor. 🗳 Primary election: Tues, May 22, 2018. Join #TeamAbrams ➡️ https://t.co/gXFm37r6mP",
+					"url": "https://t.co/pBHcZNcWF8",
+					"entities": {
+						"url": {
+							"urls": [
+								{
+									"url": "https://t.co/pBHcZNcWF8",
+									"expanded_url": "http://www.staceyabrams.com",
+									"display_url": "staceyabrams.com",
+									"indices": [
+										0,
+										23
+									]
+								}
+							]
+						},
+						"description": {
+							"urls": [
+								{
+									"url": "https://t.co/gXFm37r6mP",
+									"expanded_url": "http://bit.ly/TeamAbrams",
+									"display_url": "bit.ly/TeamAbrams",
+									"indices": [
+										133,
+										156
+									]
+								}
+							]
+						}
+					},
+					"protected": false,
+					"followers_count": 2445,
+					"friends_count": 71,
+					"listed_count": 31,
+					"created_at": "Tue Jun 27 18:39:59 +0000 2017",
+					"favourites_count": 2072,
+					"utc_offset": null,
+					"time_zone": null,
+					"geo_enabled": true,
+					"verified": false,
+					"statuses_count": 3726,
+					"lang": "en",
+					"contributors_enabled": false,
+					"is_translator": false,
+					"is_translation_enabled": false,
+					"profile_background_color": "F5F8FA",
+					"profile_background_image_url": null,
+					"profile_background_image_url_https": null,
+					"profile_background_tile": false,
+					"profile_image_url": "http://pbs.twimg.com/profile_images/920695391498194945/9L4-QCof_normal.jpg",
+					"profile_image_url_https": "https://pbs.twimg.com/profile_images/920695391498194945/9L4-QCof_normal.jpg",
+					"profile_banner_url": "https://pbs.twimg.com/profile_banners/879771304316071938/1498851368",
+					"profile_link_color": "1DA1F2",
+					"profile_sidebar_border_color": "C0DEED",
+					"profile_sidebar_fill_color": "DDEEF6",
+					"profile_text_color": "333333",
+					"profile_use_background_image": true,
+					"has_extended_profile": false,
+					"default_profile": true,
+					"default_profile_image": false,
+					"following": false,
+					"follow_request_sent": false,
+					"notifications": false,
+					"translator_type": "none"
+				},
+				"geo": null,
+				"coordinates": null,
+				"place": null,
+				"contributors": null,
+				"is_quote_status": false,
+				"retweet_count": 45,
+				"favorite_count": 81,
+				"favorited": false,
+				"retweeted": false,
+				"possibly_sensitive": false,
+				"lang": "en"
+			},
+			"retweet_count": 4,
+			"favorite_count": 4,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": true,
+		"quoted_status_id": 990954450985652200,
+		"quoted_status_id_str": "990954450985652224",
+		"retweet_count": 4,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]
