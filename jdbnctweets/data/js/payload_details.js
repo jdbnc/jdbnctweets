c5729e7f56c06e28eb6323249b17fa59c3603953
John Bence <johndbence@gmail.com>
2018-08-15 23:19:36 -0400

    Added 3 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3472,
-	"created_at": "Wed Aug 15 03:19:34 +0000 2018",
+	"tweets": 3475,
+	"created_at": "Thu Aug 16 03:19:34 +0000 2018",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2018_08.js
+++ jdbnctweets/data/js/tweets/2018_08.js
@@ -1,6 +1,664 @@
 Grailbird.data.tweets_2018_08 = 
 [
 	{
+		"created_at": "Wed Aug 15 20:06:02 +0000 2018",
+		"id": 1029821538680361000,
+		"id_str": "1029821538680360960",
+		"text": "RT @DollyParton: When the chicken and dumplings are ready! 🤠 https://t.co/4KG1VjnpeY",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DollyParton",
+					"name": "Dolly Parton",
+					"id": 14790966,
+					"id_str": "14790966",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1029821158223372300,
+					"id_str": "1029821158223372288",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Dkqo4x4V4AAc5b_.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Dkqo4x4V4AAc5b_.jpg",
+					"url": "https://t.co/4KG1VjnpeY",
+					"display_url": "pic.twitter.com/4KG1VjnpeY",
+					"expanded_url": "https://twitter.com/DollyParton/status/1029821172085665793/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 268,
+							"h": 188,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 268,
+							"h": 188,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 268,
+							"h": 188,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1029821172085665800,
+					"source_status_id_str": "1029821172085665793",
+					"source_user_id": 14790966,
+					"source_user_id_str": "14790966"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1029821158223372300,
+					"id_str": "1029821158223372288",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Dkqo4x4V4AAc5b_.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Dkqo4x4V4AAc5b_.jpg",
+					"url": "https://t.co/4KG1VjnpeY",
+					"display_url": "pic.twitter.com/4KG1VjnpeY",
+					"expanded_url": "https://twitter.com/DollyParton/status/1029821172085665793/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 268,
+							"h": 188,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 268,
+							"h": 188,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 268,
+							"h": 188,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1029821172085665800,
+					"source_status_id_str": "1029821172085665793",
+					"source_user_id": 14790966,
+					"source_user_id_str": "14790966",
+					"video_info": {
+						"aspect_ratio": [
+							67,
+							47
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/Dkqo4x4V4AAc5b_.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "University Archivist in @EmoryRoseMARBL at @EmoryUniversity tweeting about who knows what. s/o to bee girl.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 654,
+			"friends_count": 790,
+			"listed_count": 32,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 2290,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3399,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1017868834332725249/MTKWVFqs_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1017868834332725249/MTKWVFqs_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1508601718",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Aug 15 20:04:35 +0000 2018",
+			"id": 1029821172085665800,
+			"id_str": "1029821172085665793",
+			"text": "When the chicken and dumplings are ready! 🤠 https://t.co/4KG1VjnpeY",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1029821158223372300,
+						"id_str": "1029821158223372288",
+						"indices": [
+							44,
+							67
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/Dkqo4x4V4AAc5b_.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Dkqo4x4V4AAc5b_.jpg",
+						"url": "https://t.co/4KG1VjnpeY",
+						"display_url": "pic.twitter.com/4KG1VjnpeY",
+						"expanded_url": "https://twitter.com/DollyParton/status/1029821172085665793/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 268,
+								"h": 188,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 268,
+								"h": 188,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 268,
+								"h": 188,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1029821158223372300,
+						"id_str": "1029821158223372288",
+						"indices": [
+							44,
+							67
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/Dkqo4x4V4AAc5b_.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Dkqo4x4V4AAc5b_.jpg",
+						"url": "https://t.co/4KG1VjnpeY",
+						"display_url": "pic.twitter.com/4KG1VjnpeY",
+						"expanded_url": "https://twitter.com/DollyParton/status/1029821172085665793/photo/1",
+						"type": "animated_gif",
+						"sizes": {
+							"medium": {
+								"w": 268,
+								"h": 188,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 268,
+								"h": 188,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 268,
+								"h": 188,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								67,
+								47
+							],
+							"variants": [
+								{
+									"bitrate": 0,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/tweet_video/Dkqo4x4V4AAc5b_.mp4"
+								}
+							]
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.heyorca.com\" rel=\"nofollow\">HeyOrca</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14790966,
+				"id_str": "14790966",
+				"name": "Dolly Parton",
+				"screen_name": "DollyParton",
+				"location": "Nashville, Tennessee",
+				"description": ".... The Official Twitter for Dolly Parton ....     I Believe In You 🦋 Available Now!",
+				"url": "https://t.co/SWOJBPzelA",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/SWOJBPzelA",
+								"expanded_url": "http://smarturl.it/dollyibelieveinyou",
+								"display_url": "smarturl.it/dollyibelievei…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4665618,
+				"friends_count": 39,
+				"listed_count": 15401,
+				"created_at": "Thu May 15 20:00:06 +0000 2008",
+				"favourites_count": 241,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 1714,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/900421057013846016/aRe2bPxe_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/900421057013846016/aRe2bPxe_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790966/1507867438",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "8DD1E6",
+				"profile_text_color": "362720",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1793,
+			"favorite_count": 7809,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1793,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Wed Aug 15 20:03:49 +0000 2018",
+		"id": 1029820979302867000,
+		"id_str": "1029820979302866950",
+		"text": "Comment @ https://t.co/T3GAaKVPhX",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/T3GAaKVPhX",
+					"expanded_url": "https://github.com/saa-ts-dacs/dacs/pull/20",
+					"display_url": "github.com/saa-ts-dacs/da…",
+					"indices": [
+						10,
+						33
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1029820626440257500,
+		"in_reply_to_status_id_str": "1029820626440257536",
+		"in_reply_to_user_id": 24188638,
+		"in_reply_to_user_id_str": "24188638",
+		"in_reply_to_screen_name": "jdbence",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "University Archivist in @EmoryRoseMARBL at @EmoryUniversity tweeting about who knows what. s/o to bee girl.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 654,
+			"friends_count": 790,
+			"listed_count": 32,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 2290,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3399,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1017868834332725249/MTKWVFqs_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1017868834332725249/MTKWVFqs_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1508601718",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr"
+	},
+	{
+		"created_at": "Wed Aug 15 20:02:25 +0000 2018",
+		"id": 1029820626440257500,
+		"id_str": "1029820626440257536",
+		"text": "Do you, #saa18? If so, comment! https://t.co/6Wj2QeQXvM",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "saa18",
+					"indices": [
+						8,
+						14
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1029820563332587500,
+					"id_str": "1029820563332587520",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/DkqoWJvU4AAyX9-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DkqoWJvU4AAyX9-.jpg",
+					"url": "https://t.co/6Wj2QeQXvM",
+					"display_url": "pic.twitter.com/6Wj2QeQXvM",
+					"expanded_url": "https://twitter.com/jdbence/status/1029820626440257536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1029820563332587500,
+					"id_str": "1029820563332587520",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/DkqoWJvU4AAyX9-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DkqoWJvU4AAyX9-.jpg",
+					"url": "https://t.co/6Wj2QeQXvM",
+					"display_url": "pic.twitter.com/6Wj2QeQXvM",
+					"expanded_url": "https://twitter.com/jdbence/status/1029820626440257536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1029820564343377900,
+					"id_str": "1029820564343377920",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/DkqoWNgUUAA2oJU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DkqoWNgUUAA2oJU.jpg",
+					"url": "https://t.co/6Wj2QeQXvM",
+					"display_url": "pic.twitter.com/6Wj2QeQXvM",
+					"expanded_url": "https://twitter.com/jdbence/status/1029820626440257536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "University Archivist in @EmoryRoseMARBL at @EmoryUniversity tweeting about who knows what. s/o to bee girl.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 654,
+			"friends_count": 790,
+			"listed_count": 32,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 2290,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3399,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1017868834332725249/MTKWVFqs_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1017868834332725249/MTKWVFqs_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1508601718",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Aug 14 17:39:24 +0000 2018",
 		"id": 1029422250330677200,
 		"id_str": "1029422250330677248",
