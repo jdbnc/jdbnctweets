d865402dbe6add9ccfe86e7496274528c5449c38
John Bence <johndbence@gmail.com>
2018-11-04 22:19:35 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3520,
-	"created_at": "Sun Nov 04 03:19:34 +0000 2018",
+	"tweets": 3521,
+	"created_at": "Mon Nov 05 03:19:34 +0000 2018",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2018_11.js
+++ jdbnctweets/data/js/tweets/2018_11.js
@@ -88,6 +88,174 @@ Grailbird.data.tweets_2018_11 =
 				}
 			},
 			"protected": false,
+			"followers_count": 19301,
+			"friends_count": 899,
+			"listed_count": 685,
+			"created_at": "Thu Oct 02 17:50:51 +0000 2008",
+			"favourites_count": 11512,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 30338,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/856710433100754949/u6qeh8sO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/856710433100754949/u6qeh8sO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16563015/1482726357",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "9c9fa69deb120a34",
+			"url": "https://api.twitter.com/1.1/geo/id/9c9fa69deb120a34.json",
+			"place_type": "city",
+			"name": "Dunwoody",
+			"full_name": "Dunwoody, GA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-84.348046,
+							33.915238
+						],
+						[
+							-84.2649311,
+							33.915238
+						],
+						[
+							-84.2649311,
+							33.970907
+						],
+						[
+							-84.348046,
+							33.970907
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 22,
+		"favorite_count": 18,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Says @wsbtv news director @mistiwsb: “We regret that we had to cancel but once Secretary Kemp pulled out at the last minute, the candidates could not agree to a new time.” Full story here #gapol https://t.co/m3GpJGqH1a"
+	},
+	{
+		"created_at": "Thu Nov 01 00:13:04 +0000 2018",
+		"id": 1057787573395972100,
+		"id_str": "1057787573395972096",
+		"full_text": "Says @wsbtv news director @mistiwsb: “We regret that we had to cancel but once Secretary Kemp pulled out at the last minute, the candidates could not agree to a new time.” Full story here #gapol https://t.co/m3GpJGqH1a",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			218
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "gapol",
+					"indices": [
+						188,
+						194
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wsbtv",
+					"name": "WSB-TV",
+					"id": 21094888,
+					"id_str": "21094888",
+					"indices": [
+						5,
+						11
+					]
+				},
+				{
+					"screen_name": "mistiwsb",
+					"name": "Misti Turnbull",
+					"id": 32541958,
+					"id_str": "32541958",
+					"indices": [
+						26,
+						35
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/m3GpJGqH1a",
+					"expanded_url": "https://www.wsbtv.com/news/local/kemp-unable-to-debate-as-scheduled-alternatives-being-considered/863845404",
+					"display_url": "wsbtv.com/news/local/kem…",
+					"indices": [
+						195,
+						218
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1057787124823588900,
+		"in_reply_to_status_id_str": "1057787124823588864",
+		"in_reply_to_user_id": 16563015,
+		"in_reply_to_user_id_str": "16563015",
+		"in_reply_to_screen_name": "bluestein",
+		"user": {
+			"id": 16563015,
+			"id_str": "16563015",
+			"name": "Greg Bluestein",
+			"screen_name": "bluestein",
+			"location": "ATL",
+			"description": "@AJC political reporter. Husband, father, aspiring mensch. Atlanta native working to tell my city's story. Also at @PoliticallyGa. gbluestein@ajc.com",
+			"url": "https://t.co/GpctsyppXF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GpctsyppXF",
+							"expanded_url": "http://bit.ly/2i3Dsyn",
+							"display_url": "bit.ly/2i3Dsyn",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 19199,
 			"friends_count": 899,
 			"listed_count": 667,
