3ef90af8b493aa0ca369ca3d26a9a3228e6098ef
John Bence <johndbence@gmail.com>
2018-11-06 22:19:36 -0500

    Added 2 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3521,
-	"created_at": "Mon Nov 05 03:19:34 +0000 2018",
+	"tweets": 3523,
+	"created_at": "Wed Nov 07 03:19:35 +0000 2018",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2018_11.js
+++ jdbnctweets/data/js/tweets/2018_11.js
@@ -1,6 +1,465 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Tue Nov 06 23:13:01 +0000 2018",
+		"id": 1059946786515759100,
+		"id_str": "1059946786515759107",
+		"full_text": "GOP gubernatorial candidate Brian Kemp also had a voting issue today. When he tried to vote, his voter card said \"invalid.\" Our @DaveHWSB was with Kemp today as he voted and is covering the campaign all night. #ElectionOn2 https://t.co/d4yktfco6v https://t.co/YR9FeJUijj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			246
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ElectionOn2",
+					"indices": [
+						210,
+						222
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DaveHWSB",
+					"name": "Dave Huddleston",
+					"id": 25377866,
+					"id_str": "25377866",
+					"indices": [
+						128,
+						137
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/d4yktfco6v",
+					"expanded_url": "https://2wsb.tv/2PFWKvC",
+					"display_url": "2wsb.tv/2PFWKvC",
+					"indices": [
+						223,
+						246
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1059946707285409800,
+					"id_str": "1059946707285409792",
+					"indices": [
+						247,
+						270
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DrWv6hiXgAAKZ58.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DrWv6hiXgAAKZ58.jpg",
+					"url": "https://t.co/YR9FeJUijj",
+					"display_url": "pic.twitter.com/YR9FeJUijj",
+					"expanded_url": "https://twitter.com/wsbtv/status/1059946786515759107/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1059946707285409800,
+					"id_str": "1059946707285409792",
+					"indices": [
+						247,
+						270
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DrWv6hiXgAAKZ58.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DrWv6hiXgAAKZ58.jpg",
+					"url": "https://t.co/YR9FeJUijj",
+					"display_url": "pic.twitter.com/YR9FeJUijj",
+					"expanded_url": "https://twitter.com/wsbtv/status/1059946786515759107/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/DrWv6hiXgAAKZ58.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21094888,
+			"id_str": "21094888",
+			"name": "WSB-TV",
+			"screen_name": "wsbtv",
+			"location": "Atlanta, GA",
+			"description": "Metro Atlanta and north Georgia's #1 source for breaking news, weather, and traffic coverage you can count on. https://t.co/eNZKwAfuqw",
+			"url": "https://t.co/Bep9V2hmhQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Bep9V2hmhQ",
+							"expanded_url": "http://2wsb.tv/News2Go",
+							"display_url": "2wsb.tv/News2Go",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/eNZKwAfuqw",
+							"expanded_url": "http://Instagram.com/wsbtv",
+							"display_url": "Instagram.com/wsbtv",
+							"indices": [
+								111,
+								134
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1160552,
+			"friends_count": 2261,
+			"listed_count": 3385,
+			"created_at": "Tue Feb 17 15:00:01 +0000 2009",
+			"favourites_count": 9638,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 457822,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "151A7E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/951133954874249217/1X0mD6x__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/951133954874249217/1X0mD6x__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21094888/1521547649",
+			"profile_link_color": "151A7E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1646,
+		"favorite_count": 2814,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "GOP gubernatorial candidate Brian Kemp also had a voting issue today. When he tried to vote, his voter card said \"invalid.\" Our @DaveHWSB was with Kemp today as he voted and is covering the campaign all night. #ElectionOn2 https://t.co/d4yktfco6v https://t.co/YR9FeJUijj"
+	},
+	{
+		"created_at": "Wed Nov 07 01:36:15 +0000 2018",
+		"id": 1059982834759336000,
+		"id_str": "1059982834759335936",
+		"full_text": "Annistown Elementary School open till 9:25 pm. I was just there. No line, and there are cookies. VOTE! https://t.co/O2xziHJjK8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			102
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/O2xziHJjK8",
+					"expanded_url": "https://twitter.com/AtlantaMagazine/status/1059982119223595009",
+					"display_url": "twitter.com/AtlantaMagazin…",
+					"indices": [
+						103,
+						126
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1426557763,
+			"id_str": "1426557763",
+			"name": "mary dudziak",
+			"screen_name": "marydudziak",
+			"location": "out on a trail",
+			"description": "Emory law prof & historian. Obsessed with archives. Writing about how American war lost its politics. \n\nWhatever your interests, please vote on November 6.",
+			"url": "https://t.co/jF1w2Hl7HE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jF1w2Hl7HE",
+							"expanded_url": "http://law.emory.edu/faculty-and-scholarship/faculty-profiles/dudziak-profile.html",
+							"display_url": "law.emory.edu/faculty-and-sc…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7655,
+			"friends_count": 2438,
+			"listed_count": 194,
+			"created_at": "Mon May 13 22:46:10 +0000 2013",
+			"favourites_count": 16325,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 18122,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ACDED6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1036732906792579072/dp5ENOsy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1036732906792579072/dp5ENOsy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1426557763/1434998574",
+			"profile_link_color": "038543",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "F6F6F6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1059982119223595000,
+		"quoted_status_id_str": "1059982119223595009",
+		"quoted_status_permalink": {
+			"url": "https://t.co/O2xziHJjK8",
+			"expanded": "https://twitter.com/AtlantaMagazine/status/1059982119223595009",
+			"display": "twitter.com/AtlantaMagazin…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Nov 07 01:33:25 +0000 2018",
+			"id": 1059982119223595000,
+			"id_str": "1059982119223595009",
+			"full_text": "Several metro #Atlanta polling places have extended hours tonight after issues:\n\nPittman Park Rec Center: 9pm\n\nBooker T. Washington HS: 10pm\n\nMorehouse College (Archer): 10pm\n\nAnnistown Elem, Snellville: 9:25pm https://t.co/yNpP4D3aDJ",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				210
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Atlanta",
+						"indices": [
+							14,
+							22
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/yNpP4D3aDJ",
+						"expanded_url": "https://twitter.com/thomaswheatley/status/1059973554660360192",
+						"display_url": "twitter.com/thomaswheatley…",
+						"indices": [
+							211,
+							234
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 45642027,
+				"id_str": "45642027",
+				"name": "Atlanta Magazine",
+				"screen_name": "AtlantaMagazine",
+				"location": "Atlanta, GA",
+				"description": "Covering food, style, entertainment, and all things #Atlanta since 1961; tweeting about them since 2009",
+				"url": "http://t.co/ES08o9A1Ca",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/ES08o9A1Ca",
+								"expanded_url": "http://atlantamagazine.com",
+								"display_url": "atlantamagazine.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 498509,
+				"friends_count": 308,
+				"listed_count": 1643,
+				"created_at": "Mon Jun 08 19:08:33 +0000 2009",
+				"favourites_count": 190,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 24963,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "800000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/760479443919990784/1ce0dkXQ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/760479443919990784/1ce0dkXQ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/45642027/1401730820",
+				"profile_link_color": "800000",
+				"profile_sidebar_border_color": "9E9E9E",
+				"profile_sidebar_fill_color": "D1D1D1",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1059973554660360200,
+			"quoted_status_id_str": "1059973554660360192",
+			"quoted_status_permalink": {
+				"url": "https://t.co/yNpP4D3aDJ",
+				"expanded": "https://twitter.com/thomaswheatley/status/1059973554660360192",
+				"display": "twitter.com/thomaswheatley…"
+			},
+			"retweet_count": 215,
+			"favorite_count": 113,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Annistown Elementary School open till 9:25 pm. I was just there. No line, and there are cookies. VOTE! https://t.co/O2xziHJjK8"
+	},
+	{
 		"created_at": "Thu Nov 01 00:13:04 +0000 2018",
 		"id": 1057787573395972100,
 		"id_str": "1057787573395972096",
