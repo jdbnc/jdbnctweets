f903e5379a1ea9e98d6ffb4ac7863523ec56506a
John Bence <johndbence@gmail.com>
2018-11-24 22:19:36 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3529,
-	"created_at": "Sat Nov 10 03:19:35 +0000 2018",
+	"tweets": 3530,
+	"created_at": "Sun Nov 25 03:19:35 +0000 2018",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2018_11.js
+++ jdbnctweets/data/js/tweets/2018_11.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Sat Nov 24 21:28:10 +0000 2018",
+		"id": 1066443380757094400,
+		"id_str": "1066443380757094401",
+		"full_text": "@cowboycourtney https://t.co/tfAUyIuRHO",
+		"truncated": false,
+		"display_text_range": [
+			15,
+			15
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "cowboycourtney",
+					"name": "Creative Negativism",
+					"id": 114779832,
+					"id_str": "114779832",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1066443346644750300,
+					"id_str": "1066443346644750336",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DszEktHW0AAY_Oz.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DszEktHW0AAY_Oz.jpg",
+					"url": "https://t.co/tfAUyIuRHO",
+					"display_url": "pic.twitter.com/tfAUyIuRHO",
+					"expanded_url": "https://twitter.com/jdbence/status/1066443380757094401/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 400,
+							"h": 282,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 282,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 282,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1066443346644750300,
+					"id_str": "1066443346644750336",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DszEktHW0AAY_Oz.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DszEktHW0AAY_Oz.jpg",
+					"url": "https://t.co/tfAUyIuRHO",
+					"display_url": "pic.twitter.com/tfAUyIuRHO",
+					"expanded_url": "https://twitter.com/jdbence/status/1066443380757094401/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 400,
+							"h": 282,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 282,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 282,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							200,
+							141
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/DszEktHW0AAY_Oz.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1066439300382900200,
+		"in_reply_to_status_id_str": "1066439300382900225",
+		"in_reply_to_user_id": 114779832,
+		"in_reply_to_user_id_str": "114779832",
+		"in_reply_to_screen_name": "cowboycourtney",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "University Archivist in @EmoryRoseMARBL at @EmoryUniversity tweeting about who knows what.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 663,
+			"friends_count": 802,
+			"listed_count": 33,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 2493,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3435,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1066434587272904704/8wKQJfRS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1066434587272904704/8wKQJfRS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1540341710",
+			"profile_link_color": "125408",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@cowboycourtney https://t.co/tfAUyIuRHO"
+	},
+	{
 		"created_at": "Fri Nov 09 19:35:03 +0000 2018",
 		"id": 1060979098590347300,
 		"id_str": "1060979098590347265",
