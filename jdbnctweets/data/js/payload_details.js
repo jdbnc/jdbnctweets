f077c58c4dd8347808630bea66bd3bd4668bf066
John Bence <johndbence@gmail.com>
2019-04-08 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3552,
-	"created_at": "Fri Apr 05 03:19:34 +0000 2019",
+	"tweets": 3553,
+	"created_at": "Tue Apr 09 03:19:35 +0000 2019",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2019_04.js
+++ jdbnctweets/data/js/tweets/2019_04.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 08 14:03:48 +0000 2019",
+		"id": 1115253914553790500,
+		"id_str": "1115253914553790465",
+		"full_text": "Grateful for the chance to talk to Adwoa Afful @BitchMedia and to my colleague @jp_mongeau for putting us in touch! #webarchiving https://t.co/SilkTtEi13",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			129
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "webarchiving",
+					"indices": [
+						116,
+						129
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BitchMedia",
+					"name": "BitchMedia",
+					"id": 16314021,
+					"id_str": "16314021",
+					"indices": [
+						47,
+						58
+					]
+				},
+				{
+					"screen_name": "jp_mongeau",
+					"name": "Philippe Mongeau",
+					"id": 773348868863959000,
+					"id_str": "773348868863959044",
+					"indices": [
+						79,
+						90
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/SilkTtEi13",
+					"expanded_url": "https://twitter.com/BitchMedia/status/1114664083087876097",
+					"display_url": "twitter.com/BitchMedia/sta…",
+					"indices": [
+						130,
+						153
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "University Archivist in @EmoryRoseMARBL at @EmoryUniversity tweeting about who knows what.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 674,
+			"friends_count": 807,
+			"listed_count": 34,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 2592,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3443,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1068513713962065920/1gVdC8YJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1068513713962065920/1gVdC8YJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1545266925",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1114664083087876100,
+		"quoted_status_id_str": "1114664083087876097",
+		"quoted_status_permalink": {
+			"url": "https://t.co/SilkTtEi13",
+			"expanded": "https://twitter.com/BitchMedia/status/1114664083087876097",
+			"display": "twitter.com/BitchMedia/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Apr 06 23:00:01 +0000 2019",
+			"id": 1114664083087876100,
+			"id_str": "1114664083087876097",
+			"full_text": "Last month it was reported that MySpace had a server-migration snafu that resulted in the loss of all music, video, and photos uploaded to the site between 2003 and 2015. https://t.co/osteiJNgAB",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				194
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/osteiJNgAB",
+						"expanded_url": "https://buff.ly/2TS5PzE",
+						"display_url": "buff.ly/2TS5PzE",
+						"indices": [
+							171,
+							194
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16314021,
+				"id_str": "16314021",
+				"name": "BitchMedia",
+				"screen_name": "BitchMedia",
+				"location": "Portland, OR",
+				"description": "Feminist Response to Pop Culture: Bitch magazine, Popaganda, Backtalk, Feminist Snack Break, & Bitch on Campus. Also: Quarterly Writing Fellowships.",
+				"url": "https://t.co/g5GDtc8uR4",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/g5GDtc8uR4",
+								"expanded_url": "http://bitchmedia.org",
+								"display_url": "bitchmedia.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 204691,
+				"friends_count": 6869,
+				"listed_count": 4541,
+				"created_at": "Tue Sep 16 17:55:24 +0000 2008",
+				"favourites_count": 8429,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 30455,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "BADFCD",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1090301099226030085/vdYfc5NW_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1090301099226030085/vdYfc5NW_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16314021/1548782991",
+				"profile_link_color": "339966",
+				"profile_sidebar_border_color": "FF005D",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "0C3E53",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 11,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 3,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Grateful for the chance to talk to Adwoa Afful @BitchMedia and to my colleague @jp_mongeau for putting us in touch! #webarchiving https://t.co/SilkTtEi13"
+	},
+	{
 		"created_at": "Thu Apr 04 21:50:47 +0000 2019",
 		"id": 1113921882724548600,
 		"id_str": "1113921882724548608",
