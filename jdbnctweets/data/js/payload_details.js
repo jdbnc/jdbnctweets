02a52ffa8cdf91c305d9a0a133fe874628caa351
John Bence <johndbence@gmail.com>
2019-05-09 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3555,
-	"created_at": "Wed Apr 24 03:19:34 +0000 2019",
+	"tweets": 3556,
+	"created_at": "Fri May 10 03:19:35 +0000 2019",
 	"lang": "en"
 }
---
+++ jdbnctweets/data/js/tweets/2019_05.js
@@ -0,0 +1,96 @@
+Grailbird.data.tweets_2019_05 = 
+[
+	{
+		"created_at": "Thu May 09 21:08:14 +0000 2019",
+		"id": 1126594752500129800,
+		"id_str": "1126594752500129792",
+		"full_text": "@thericeman Two syllables.",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			26
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "thericeman",
+					"name": "Sam Reisman",
+					"id": 21126648,
+					"id_str": "21126648",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1126590845782827000,
+		"in_reply_to_status_id_str": "1126590845782827008",
+		"in_reply_to_user_id": 21126648,
+		"in_reply_to_user_id_str": "21126648",
+		"in_reply_to_screen_name": "thericeman",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "University Archivist in @EmoryRoseMARBL at @EmoryUniversity tweeting about who knows what.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 677,
+			"friends_count": 810,
+			"listed_count": 34,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 2633,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3445,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1068513713962065920/1gVdC8YJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1068513713962065920/1gVdC8YJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1545266925",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@thericeman Two syllables."
+	}
+]
