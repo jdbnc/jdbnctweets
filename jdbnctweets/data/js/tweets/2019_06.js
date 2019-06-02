fbbb96703ec1ec4b97c55a751ccfe2646c79f7b5
John Bence <johndbence@gmail.com>
2019-06-01 23:19:36 -0400

    Added 1 tweets

---
+++ jdbnctweets/data/js/tweets/2019_06.js
@@ -0,0 +1,106 @@
+Grailbird.data.tweets_2019_06 = 
+[
+	{
+		"created_at": "Sun Jun 02 00:36:20 +0000 2019",
+		"id": 1134982041378984000,
+		"id_str": "1134982041378983936",
+		"full_text": "@Duke_of_neural @22b Sigh?",
+		"truncated": false,
+		"display_text_range": [
+			21,
+			26
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Duke_of_neural",
+					"name": "ₚₕᵢₗ ₛₚₑₐᵣ 🥉",
+					"id": 1013705742,
+					"id_str": "1013705742",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "22b",
+					"name": "matthew the librarian",
+					"id": 15869446,
+					"id_str": "15869446",
+					"indices": [
+						16,
+						20
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1134644351315849200,
+		"in_reply_to_status_id_str": "1134644351315849216",
+		"in_reply_to_user_id": 1013705742,
+		"in_reply_to_user_id_str": "1013705742",
+		"in_reply_to_screen_name": "Duke_of_neural",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "University Archivist in @EmoryRoseMARBL at @EmoryUniversity. My formative years were heavily influenced by Sister Act and Godzilla.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 679,
+			"friends_count": 813,
+			"listed_count": 33,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 2665,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3450,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1068513713962065920/1gVdC8YJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1068513713962065920/1gVdC8YJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1545266925",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Duke_of_neural @22b Sigh?"
+	}
+]
