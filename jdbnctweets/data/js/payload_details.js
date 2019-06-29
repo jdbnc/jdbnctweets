2f41b5eeb1761a17d328cd8c59f028e874c670ee
John Bence <johndbence@gmail.com>
2019-06-28 23:19:35 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3579,
-	"created_at": "Fri Jun 28 03:19:35 +0000 2019",
+	"tweets": 3580,
+	"created_at": "Sat Jun 29 03:19:34 +0000 2019",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2019_06.js
+++ jdbnctweets/data/js/tweets/2019_06.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Fri Jun 28 15:36:51 +0000 2019",
+		"id": 1144630748374347800,
+		"id_str": "1144630748374347776",
+		"full_text": "@NearlySoExalted Genius writing in the beginning too: *No Mercy drum beat opening, chorus starts* \"Where do you go when you want todays HOTTEST dance hits on one unbelievable album?!\"",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			183
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NearlySoExalted",
+					"name": "Alex",
+					"id": 496406962,
+					"id_str": "496406962",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1144628793992253400,
+		"in_reply_to_status_id_str": "1144628793992253440",
+		"in_reply_to_user_id": 496406962,
+		"in_reply_to_user_id_str": "496406962",
+		"in_reply_to_screen_name": "NearlySoExalted",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "University Archivist in @EmoryRoseMARBL at @EmoryUniversity",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 681,
+			"friends_count": 816,
+			"listed_count": 32,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 2741,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3468,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1068513713962065920/1gVdC8YJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1068513713962065920/1gVdC8YJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1545266925",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@NearlySoExalted Genius writing in the beginning too: *No Mercy drum beat opening, chorus starts* \"Where do you go when you want todays HOTTEST dance hits on one unbelievable album?!\""
+	},
+	{
 		"created_at": "Thu Jun 27 15:26:28 +0000 2019",
 		"id": 1144265746710782000,
 		"id_str": "1144265746710781952",
