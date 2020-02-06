d20ef4c2313ff823a1655c2e2eaadfa06eb81d42
John Bence <johndbence@gmail.com>
2020-02-05 22:19:36 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3639,
-	"created_at": "Wed Jan 29 03:19:36 +0000 2020",
+	"tweets": 3640,
+	"created_at": "Thu Feb 06 03:19:34 +0000 2020",
 	"lang": "en"
 }
---
+++ jdbnctweets/data/js/tweets/2020_02.js
@@ -0,0 +1,318 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Thu Feb 06 00:13:44 +0000 2020",
+		"id": 1225210932198039600,
+		"id_str": "1225210932198039554",
+		"full_text": "It’s always fun getting to work with @marydudziak’s Equality at Emory class! https://t.co/WaInrooP5a",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			76
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "marydudziak",
+					"name": "mary dudziak",
+					"id": 1426557763,
+					"id_str": "1426557763",
+					"indices": [
+						37,
+						49
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/WaInrooP5a",
+					"expanded_url": "https://twitter.com/marydudziak/status/1225198154825510914",
+					"display_url": "twitter.com/marydudziak/st…",
+					"indices": [
+						77,
+						100
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "Archivist. Heavily influenced By Godzilla and Sister Act. #atlutd #saintsfc #schalke",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 675,
+			"friends_count": 856,
+			"listed_count": 32,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 3053,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3530,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1068513713962065920/1gVdC8YJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1068513713962065920/1gVdC8YJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1563498606",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1225198154825511000,
+		"quoted_status_id_str": "1225198154825510914",
+		"quoted_status_permalink": {
+			"url": "https://t.co/WaInrooP5a",
+			"expanded": "https://twitter.com/marydudziak/status/1225198154825510914",
+			"display": "twitter.com/marydudziak/st…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Feb 05 23:22:57 +0000 2020",
+			"id": 1225198154825511000,
+			"id_str": "1225198154825510914",
+			"full_text": "Thanks to @jdbence at @EmoryRoseMARBL my @EmoryLaw students got hands on experience with how to use the archives. https://t.co/p1uiDLgN3i",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				113
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "jdbence",
+						"name": "John Bence",
+						"id": 24188638,
+						"id_str": "24188638",
+						"indices": [
+							10,
+							18
+						]
+					},
+					{
+						"screen_name": "EmoryRoseMARBL",
+						"name": "Rose Library Emory",
+						"id": 2979785051,
+						"id_str": "2979785051",
+						"indices": [
+							22,
+							37
+						]
+					},
+					{
+						"screen_name": "EmoryLaw",
+						"name": "Emory Law",
+						"id": 16032593,
+						"id_str": "16032593",
+						"indices": [
+							41,
+							50
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1225198145551945700,
+						"id_str": "1225198145551945728",
+						"indices": [
+							114,
+							137
+						],
+						"media_url": "http://pbs.twimg.com/media/EQDHONUXUAAH7-C.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EQDHONUXUAAH7-C.jpg",
+						"url": "https://t.co/p1uiDLgN3i",
+						"display_url": "pic.twitter.com/p1uiDLgN3i",
+						"expanded_url": "https://twitter.com/marydudziak/status/1225198154825510914/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1107,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 368,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 649,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1225198145551945700,
+						"id_str": "1225198145551945728",
+						"indices": [
+							114,
+							137
+						],
+						"media_url": "http://pbs.twimg.com/media/EQDHONUXUAAH7-C.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EQDHONUXUAAH7-C.jpg",
+						"url": "https://t.co/p1uiDLgN3i",
+						"display_url": "pic.twitter.com/p1uiDLgN3i",
+						"expanded_url": "https://twitter.com/marydudziak/status/1225198154825510914/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1107,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 368,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 649,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1426557763,
+				"id_str": "1426557763",
+				"name": "mary dudziak",
+				"screen_name": "marydudziak",
+				"location": "out on a trail",
+				"description": "@EmoryUniversity law prof & historian. Obsessed with archives. Writing about how American war lost its politics. she/her/hers.",
+				"url": "https://t.co/wT8phV8hvq",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/wT8phV8hvq",
+								"expanded_url": "http://www.marydudziak.com/",
+								"display_url": "marydudziak.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 10306,
+				"friends_count": 3265,
+				"listed_count": 251,
+				"created_at": "Mon May 13 22:46:10 +0000 2013",
+				"favourites_count": 29410,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 24448,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "ACDED6",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1196424888703160322/4jVqJqy1_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1196424888703160322/4jVqJqy1_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1426557763/1561403017",
+				"profile_link_color": "038543",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "F6F6F6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 5,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It’s always fun getting to work with @marydudziak’s Equality at Emory class! https://t.co/WaInrooP5a"
+	}
+]
