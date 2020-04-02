1f6eba509b92a15b8d43779e066cf8bc08c85881
John Bence <johndbence@gmail.com>
2020-04-01 23:19:38 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3669,
-	"created_at": "Tue Mar 31 03:19:35 +0000 2020",
+	"tweets": 3670,
+	"created_at": "Thu Apr 02 03:19:36 +0000 2020",
 	"lang": "en"
 }
---
+++ jdbnctweets/data/js/tweets/2020_04.js
@@ -0,0 +1,177 @@
+Grailbird.data.tweets_2020_04 = 
+[
+	{
+		"created_at": "Wed Apr 01 15:03:49 +0000 2020",
+		"id": 1245366263288934400,
+		"id_str": "1245366263288934401",
+		"full_text": "The bummer about having a birthday on the first of the month is paying all the bills https://t.co/GB7ggPuW65",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			84
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1245366233689665500,
+					"id_str": "1245366233689665537",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EUht_cZWsAEZoLw.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EUht_cZWsAEZoLw.jpg",
+					"url": "https://t.co/GB7ggPuW65",
+					"display_url": "pic.twitter.com/GB7ggPuW65",
+					"expanded_url": "https://twitter.com/jdbence/status/1245366263288934401/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 498,
+							"h": 280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 280,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 498,
+							"h": 280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245366233689665500,
+					"id_str": "1245366233689665537",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EUht_cZWsAEZoLw.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EUht_cZWsAEZoLw.jpg",
+					"url": "https://t.co/GB7ggPuW65",
+					"display_url": "pic.twitter.com/GB7ggPuW65",
+					"expanded_url": "https://twitter.com/jdbence/status/1245366263288934401/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 498,
+							"h": 280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 280,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 498,
+							"h": 280,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							249,
+							140
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EUht_cZWsAEZoLw.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "Archivist @EmoryRoseMARBL • #SomosAtlanta #SaintsFC #s04 #NWSLtoATL",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 681,
+			"friends_count": 876,
+			"listed_count": 32,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 3237,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3562,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225734038805209088/BbP6Q1KF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225734038805209088/BbP6Q1KF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1581071742",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The bummer about having a birthday on the first of the month is paying all the bills https://t.co/GB7ggPuW65"
+	}
+]
