216f1883d7757379aff5f1b7503adb24dd56dd48
John Bence <johndbence@gmail.com>
2020-04-06 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3671,
-	"created_at": "Sat Apr 04 03:19:35 +0000 2020",
+	"tweets": 3672,
+	"created_at": "Tue Apr 07 03:19:35 +0000 2020",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2020_04.js
+++ jdbnctweets/data/js/tweets/2020_04.js
@@ -1,6 +1,280 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Mon Apr 06 13:44:09 +0000 2020",
+		"id": 1246953081495060500,
+		"id_str": "1247158153004945408",
+		"full_text": "Millennials living through their 2nd “once in a generation” economic collapse https://t.co/CSkTiwz0tB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1246628507817373700,
+					"id_str": "1246628507817373696",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1246628507817373696/pu/img/VBU-yRiB5rK-P4GM.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1246628507817373696/pu/img/VBU-yRiB5rK-P4GM.jpg",
+					"url": "https://t.co/CSkTiwz0tB",
+					"display_url": "pic.twitter.com/CSkTiwz0tB",
+					"expanded_url": "https://twitter.com/slumptmila/status/1246628536665817089/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1246628536665817000,
+					"source_status_id_str": "1246628536665817089",
+					"source_user_id": 938083271602085900,
+					"source_user_id_str": "938083271602085889"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1246628507817373700,
+					"id_str": "1246628507817373696",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1246628507817373696/pu/img/VBU-yRiB5rK-P4GM.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1246628507817373696/pu/img/VBU-yRiB5rK-P4GM.jpg",
+					"url": "https://t.co/CSkTiwz0tB",
+					"display_url": "pic.twitter.com/CSkTiwz0tB",
+					"expanded_url": "https://twitter.com/slumptmila/status/1246628536665817089/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1246628536665817000,
+					"source_status_id_str": "1246628536665817089",
+					"source_user_id": 938083271602085900,
+					"source_user_id_str": "938083271602085889",
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 6128,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1246628507817373696/pu/vid/640x360/yjtjs_vLB-KQPSr5.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1246628507817373696/pu/pl/33tjEz1zVYO-OIUz.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1246628507817373696/pu/vid/1280x720/u6CHCDZsNp2yJpUH.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1246628507817373696/pu/vid/480x270/eE6D07LdwAqmSLzC.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 938083271602085900,
+							"id_str": "938083271602085889",
+							"name": "𝓙𝓪𝓶𝓲𝓵𝓪 | 𝐹𝒶𝓈𝒽𝒾𝑜𝓃 𝑀𝑜𝒹𝑒𝓁",
+							"screen_name": "slumptmila",
+							"location": "Long Beach, CA",
+							"description": "𝘚𝘯𝘢𝘱𝘊𝘩𝘢𝘵~𝘉𝘢𝘣𝘺𝘺.𝘮𝘪𝘭a",
+							"url": "https://t.co/nGL4tV6BEx",
+							"entities": {
+								"url": {
+									"urls": [
+										{
+											"url": "https://t.co/nGL4tV6BEx",
+											"expanded_url": "https://www.instagram.com/babyy.mila/",
+											"display_url": "instagram.com/babyy.mila/",
+											"indices": [
+												0,
+												23
+											]
+										}
+									]
+								},
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 747,
+							"friends_count": 631,
+							"listed_count": 1,
+							"created_at": "Tue Dec 05 16:30:55 +0000 2017",
+							"favourites_count": 9343,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": true,
+							"verified": false,
+							"statuses_count": 2721,
+							"lang": null,
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "F5F8FA",
+							"profile_background_image_url": null,
+							"profile_background_image_url_https": null,
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/1247271914390712320/vpZdUUbJ_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/1247271914390712320/vpZdUUbJ_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/938083271602085889/1585941284",
+							"profile_link_color": "1DA1F2",
+							"profile_sidebar_border_color": "C0DEED",
+							"profile_sidebar_fill_color": "DDEEF6",
+							"profile_text_color": "333333",
+							"profile_use_background_image": true,
+							"has_extended_profile": true,
+							"default_profile": true,
+							"default_profile_image": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 248256515,
+			"id_str": "248256515",
+			"name": "Ryan Campbell",
+			"screen_name": "RyanForSmyrna",
+			"location": "Smyrna, GA",
+			"description": "Business Owner, Financial Planner, Investor, liberal.",
+			"url": "https://t.co/Ehbnr5uiM0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ehbnr5uiM0",
+							"expanded_url": "http://ryanforsmyrna.com",
+							"display_url": "ryanforsmyrna.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3782,
+			"friends_count": 2947,
+			"listed_count": 64,
+			"created_at": "Sun Feb 06 16:16:23 +0000 2011",
+			"favourites_count": 5847,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 199816,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1210049788600819712/8Rng5zXb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210049788600819712/8Rng5zXb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/248256515/1578792016",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 73481,
+		"favorite_count": 255891,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Millennials living through their 2nd “once in a generation” economic collapse https://t.co/CSkTiwz0tB"
+	},
+	{
 		"created_at": "Fri Apr 03 14:55:40 +0000 2020",
 		"id": 1246088987141439500,
 		"id_str": "1246088987141439490",
