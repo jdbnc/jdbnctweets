7a5b6bada4362a31ba8131ce6759c366727dc177
John Bence <johndbence@gmail.com>
2020-05-07 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3680,
-	"created_at": "Thu Apr 30 03:19:35 +0000 2020",
+	"tweets": 3681,
+	"created_at": "Fri May 08 03:19:35 +0000 2020",
 	"lang": "en"
 }
---
+++ jdbnctweets/data/js/tweets/2020_05.js
@@ -0,0 +1,114 @@
+Grailbird.data.tweets_2020_05 = 
+[
+	{
+		"created_at": "Thu May 07 14:42:17 +0000 2020",
+		"id": 1258406807170683000,
+		"id_str": "1258406807170682880",
+		"full_text": "@MarylandMav @rogbennett ...but in the meantime you can root for die konigsblauen #s04 🔵⚪",
+		"truncated": false,
+		"display_text_range": [
+			25,
+			89
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "s04",
+					"indices": [
+						82,
+						86
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MarylandMav",
+					"name": "MarylanDaderick",
+					"id": 145768216,
+					"id_str": "145768216",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "rogbennett",
+					"name": "roger bennett",
+					"id": 116947296,
+					"id_str": "116947296",
+					"indices": [
+						13,
+						24
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1258404461841711000,
+		"in_reply_to_status_id_str": "1258404461841711104",
+		"in_reply_to_user_id": 145768216,
+		"in_reply_to_user_id_str": "145768216",
+		"in_reply_to_screen_name": "MarylandMav",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "Archivist @EmoryRoseMARBL • #SomosAtlanta #SaintsFC #s04",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 687,
+			"friends_count": 885,
+			"listed_count": 32,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 3314,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3577,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225734038805209088/BbP6Q1KF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225734038805209088/BbP6Q1KF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1581071742",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@MarylandMav @rogbennett ...but in the meantime you can root for die konigsblauen #s04 🔵⚪"
+	}
+]
