8eff68d4df85a86009f2fc945320f01cc67d0b2e
John Bence <johndbence@gmail.com>
2020-06-02 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3698,
-	"created_at": "Mon Jun 01 03:19:35 +0000 2020",
+	"tweets": 3699,
+	"created_at": "Wed Jun 03 03:19:35 +0000 2020",
 	"lang": "en"
 }
---
+++ jdbnctweets/data/js/tweets/2020_06.js
@@ -0,0 +1,129 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Tue Jun 02 22:05:48 +0000 2020",
+		"id": 1267915040457478100,
+		"id_str": "1267940504550981632",
+		"full_text": "The Blackivists, a crew of Black archivists based in Chicago that I’m proud member of, just released these tips on how to document the uprisings without putting our people in the streets and community members in danger. https://t.co/s0cDYM0WtP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			243
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/s0cDYM0WtP",
+					"expanded_url": "https://sixtyinchesfromcenter.org/the-blackivists-five-tips-for-organizers-protestors-and-anyone-documenting-movements/",
+					"display_url": "sixtyinchesfromcenter.org/the-blackivist…",
+					"indices": [
+						220,
+						243
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 83997056,
+			"id_str": "83997056",
+			"name": "Erin",
+			"screen_name": "erinsco",
+			"location": "Chicago, IL",
+			"description": "Black queer femmeboi. police and prison abolitionist. non-neutral archivist/librarian. polyam. they/she. ABOLISH THE POLICE #fuck12",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 785,
+			"friends_count": 1368,
+			"listed_count": 14,
+			"created_at": "Wed Oct 21 04:06:19 +0000 2009",
+			"favourites_count": 15788,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4030,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1148894253726257152/7q8ujz-G_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1148894253726257152/7q8ujz-G_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/83997056/1590992807",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "1d9a5370a355ab0c",
+			"url": "https://api.twitter.com/1.1/geo/id/1d9a5370a355ab0c.json",
+			"place_type": "city",
+			"name": "Chicago",
+			"full_name": "Chicago, IL",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-87.940033,
+							41.644102
+						],
+						[
+							-87.523993,
+							41.644102
+						],
+						[
+							-87.523993,
+							42.0230669
+						],
+						[
+							-87.940033,
+							42.0230669
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 527,
+		"favorite_count": 544,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The Blackivists, a crew of Black archivists based in Chicago that I’m proud member of, just released these tips on how to document the uprisings without putting our people in the streets and community members in danger. https://t.co/s0cDYM0WtP"
+	}
+]
