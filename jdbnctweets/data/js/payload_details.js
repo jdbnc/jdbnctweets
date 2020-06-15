f6a076c91f1ecaa21e21a4f0705d0cfe509b7c38
John Bence <johndbence@gmail.com>
2020-06-14 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3708,
-	"created_at": "Sat Jun 13 03:19:35 +0000 2020",
+	"tweets": 3709,
+	"created_at": "Mon Jun 15 03:19:35 +0000 2020",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2020_06.js
+++ jdbnctweets/data/js/tweets/2020_06.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sun Jun 14 20:47:25 +0000 2020",
+		"id": 1272263004910956500,
+		"id_str": "1272269435521425408",
+		"full_text": "On Sunday, May 25, 1969, Black students demonstrated about racism on campus: one action was to interrupt Sunday services in the Chapel to make the Emory, White Christian community was too silent on issues of racism on campus. https://t.co/YaDVLXLzv6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			225
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1272261782934085600,
+					"id_str": "1272261782934085634",
+					"indices": [
+						226,
+						249
+					],
+					"media_url": "http://pbs.twimg.com/media/Eaf7WdEXYAIqrbR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eaf7WdEXYAIqrbR.jpg",
+					"url": "https://t.co/YaDVLXLzv6",
+					"display_url": "pic.twitter.com/YaDVLXLzv6",
+					"expanded_url": "https://twitter.com/EmoryRoseMARBL/status/1272263004910956547/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 974,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 552,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1663,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1272261782934085600,
+					"id_str": "1272261782934085634",
+					"indices": [
+						226,
+						249
+					],
+					"media_url": "http://pbs.twimg.com/media/Eaf7WdEXYAIqrbR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eaf7WdEXYAIqrbR.jpg",
+					"url": "https://t.co/YaDVLXLzv6",
+					"display_url": "pic.twitter.com/YaDVLXLzv6",
+					"expanded_url": "https://twitter.com/EmoryRoseMARBL/status/1272263004910956547/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 974,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 552,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1663,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2979785051,
+			"id_str": "2979785051",
+			"name": "Rose Library Emory",
+			"screen_name": "EmoryRoseMARBL",
+			"location": "Atlanta, GA",
+			"description": "Stuart A. Rose Manuscript, Archives, and Rare Book Library",
+			"url": "http://t.co/tROo8YcJcB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/tROo8YcJcB",
+							"expanded_url": "http://rose.library.emory.edu/",
+							"display_url": "rose.library.emory.edu",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1024,
+			"friends_count": 251,
+			"listed_count": 24,
+			"created_at": "Thu Jan 15 13:58:49 +0000 2015",
+			"favourites_count": 1025,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1486,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/555766102718832640/86Gr1wSI_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/555766102718832640/86Gr1wSI_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2979785051/1442582260",
+			"profile_link_color": "00267F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 14,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On Sunday, May 25, 1969, Black students demonstrated about racism on campus: one action was to interrupt Sunday services in the Chapel to make the Emory, White Christian community was too silent on issues of racism on campus. https://t.co/YaDVLXLzv6"
+	},
+	{
 		"created_at": "Fri Jun 12 15:29:28 +0000 2020",
 		"id": 1271464643240829000,
 		"id_str": "1271464643240828928",
