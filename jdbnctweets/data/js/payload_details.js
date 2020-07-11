743034b06532c2863a7ddc4531d60f366dd3181f
John Bence <johndbence@gmail.com>
2020-07-10 23:19:37 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3727,
-	"created_at": "Fri Jul 10 03:19:35 +0000 2020",
+	"tweets": 3728,
+	"created_at": "Sat Jul 11 03:19:35 +0000 2020",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2020_07.js
+++ jdbnctweets/data/js/tweets/2020_07.js
@@ -1,6 +1,288 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Fri Jul 10 23:32:08 +0000 2020",
+		"id": 1281544499030827000,
+		"id_str": "1281732969758154752",
+		"full_text": "Every image of Gowron exudes a powerful energy. RT to truly bless your timeline and banish evil spirits. https://t.co/RrN1GNR0aS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1281542683245318100,
+					"id_str": "1281542683245318144",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/Ecj0R89WkAA3c0H.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ecj0R89WkAA3c0H.png",
+					"url": "https://t.co/RrN1GNR0aS",
+					"display_url": "pic.twitter.com/RrN1GNR0aS",
+					"expanded_url": "https://twitter.com/WibDoesTweets/status/1281544499030827015/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 400,
+							"h": 306,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 400,
+							"h": 306,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 306,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1281542683245318100,
+					"id_str": "1281542683245318144",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/Ecj0R89WkAA3c0H.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ecj0R89WkAA3c0H.png",
+					"url": "https://t.co/RrN1GNR0aS",
+					"display_url": "pic.twitter.com/RrN1GNR0aS",
+					"expanded_url": "https://twitter.com/WibDoesTweets/status/1281544499030827015/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 400,
+							"h": 306,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 400,
+							"h": 306,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 306,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1281542728464109600,
+					"id_str": "1281542728464109572",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/Ecj0UlaWkAQw_M9.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ecj0UlaWkAQw_M9.png",
+					"url": "https://t.co/RrN1GNR0aS",
+					"display_url": "pic.twitter.com/RrN1GNR0aS",
+					"expanded_url": "https://twitter.com/WibDoesTweets/status/1281544499030827015/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 692,
+							"h": 530,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 692,
+							"h": 530,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 521,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1281542797099753500,
+					"id_str": "1281542797099753474",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/Ecj0YlGXYAIs4GK.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ecj0YlGXYAIs4GK.png",
+					"url": "https://t.co/RrN1GNR0aS",
+					"display_url": "pic.twitter.com/RrN1GNR0aS",
+					"expanded_url": "https://twitter.com/WibDoesTweets/status/1281544499030827015/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 692,
+							"h": 530,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 692,
+							"h": 530,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 521,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1281543790327627800,
+					"id_str": "1281543790327627778",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/Ecj1SZKWAAIBO5T.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ecj1SZKWAAIBO5T.png",
+					"url": "https://t.co/RrN1GNR0aS",
+					"display_url": "pic.twitter.com/RrN1GNR0aS",
+					"expanded_url": "https://twitter.com/WibDoesTweets/status/1281544499030827015/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 360,
+							"h": 450,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 360,
+							"h": 450,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 360,
+							"h": 450,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1262788940,
+			"id_str": "1262788940",
+			"name": "Wib 'Chainsaw' Johnson",
+			"screen_name": "WibDoesTweets",
+			"location": "England, United Kingdom",
+			"description": "Maker of videos, caster of pods, painter of tiny goblins. He/Him.",
+			"url": "https://t.co/ashIsKseEM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ashIsKseEM",
+							"expanded_url": "http://www.youtube.com/user/SnipeAndWibPlay",
+							"display_url": "youtube.com/user/SnipeAndW…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3388,
+			"friends_count": 247,
+			"listed_count": 12,
+			"created_at": "Tue Mar 12 20:16:09 +0000 2013",
+			"favourites_count": 22415,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12335,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1080536346693959680/FUSYdA7n_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1080536346693959680/FUSYdA7n_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1262788940/1435437586",
+			"profile_link_color": "383850",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1580,
+		"favorite_count": 2780,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Every image of Gowron exudes a powerful energy. RT to truly bless your timeline and banish evil spirits. https://t.co/RrN1GNR0aS"
+	},
+	{
 		"created_at": "Thu Jul 09 22:42:52 +0000 2020",
 		"id": 1281358185425784800,
 		"id_str": "1281358185425784834",
