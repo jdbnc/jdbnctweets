78c131e09503e4cd13cdae64a6a3523fcd24c62f
John Bence <johndbence@gmail.com>
2020-09-11 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3768,
-	"created_at": "Thu Sep 10 03:19:34 +0000 2020",
+	"tweets": 3769,
+	"created_at": "Sat Sep 12 03:19:34 +0000 2020",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2020_09.js
+++ jdbnctweets/data/js/tweets/2020_09.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Fri Sep 11 15:09:47 +0000 2020",
+		"id": 1304436987378761700,
+		"id_str": "1304436987378761729",
+		"full_text": "HAPPY FRIDAY! A tenant reported a leaky ceiling and a giant suburban used our parking lot to turn around, knocking a part of our retaining wall down in the process ALL BEFORE NOON.... being on the board of my small condo building is exciting. https://t.co/2gk2qUuCAr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			242
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1304436705240518700,
+					"id_str": "1304436705240518657",
+					"indices": [
+						243,
+						266
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EhpKRa_XkAEPO_y.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EhpKRa_XkAEPO_y.jpg",
+					"url": "https://t.co/2gk2qUuCAr",
+					"display_url": "pic.twitter.com/2gk2qUuCAr",
+					"expanded_url": "https://twitter.com/jdbence/status/1304436987378761729/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 498,
+							"h": 498,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 498,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 498,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1304436705240518700,
+					"id_str": "1304436705240518657",
+					"indices": [
+						243,
+						266
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EhpKRa_XkAEPO_y.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EhpKRa_XkAEPO_y.jpg",
+					"url": "https://t.co/2gk2qUuCAr",
+					"display_url": "pic.twitter.com/2gk2qUuCAr",
+					"expanded_url": "https://twitter.com/jdbence/status/1304436987378761729/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 498,
+							"h": 498,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 498,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 498,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EhpKRa_XkAEPO_y.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I'm an archivist   •   #ATLUTD #SaintsFC #s04  •  my inner soundtrack is just buy out",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 694,
+			"friends_count": 920,
+			"listed_count": 30,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 4062,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3668,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "HAPPY FRIDAY! A tenant reported a leaky ceiling and a giant suburban used our parking lot to turn around, knocking a part of our retaining wall down in the process ALL BEFORE NOON.... being on the board of my small condo building is exciting. https://t.co/2gk2qUuCAr"
+	},
+	{
 		"created_at": "Wed Sep 09 12:06:34 +0000 2020",
 		"id": 1303666102908653600,
 		"id_str": "1303666102908653568",
