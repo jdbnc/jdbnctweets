e19c89b9d08b945007e1cbe72cb1ed560bd3e0a5
John Bence <johndbence@gmail.com>
2020-09-18 23:19:39 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3769,
-	"created_at": "Sat Sep 12 03:19:34 +0000 2020",
+	"tweets": 3770,
+	"created_at": "Sat Sep 19 03:19:37 +0000 2020",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2020_09.js
+++ jdbnctweets/data/js/tweets/2020_09.js
@@ -1,6 +1,385 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Fri Sep 18 20:22:00 +0000 2020",
+		"id": 1307052273126838300,
+		"id_str": "1307052273126838273",
+		"full_text": "Not how I wanted to kickoff my Friday https://t.co/QzJrXJ9a8s https://t.co/VwlMWHQW4r",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QzJrXJ9a8s",
+					"expanded_url": "https://twitter.com/s04_us/status/1307051932251545600",
+					"display_url": "twitter.com/s04_us/status/…",
+					"indices": [
+						38,
+						61
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1307052234828640300,
+					"id_str": "1307052234828640257",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EiOVFR_XcAEsDlY.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EiOVFR_XcAEsDlY.jpg",
+					"url": "https://t.co/VwlMWHQW4r",
+					"display_url": "pic.twitter.com/VwlMWHQW4r",
+					"expanded_url": "https://twitter.com/jdbence/status/1307052273126838273/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 300,
+							"h": 300,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 300,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 300,
+							"h": 300,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1307052234828640300,
+					"id_str": "1307052234828640257",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EiOVFR_XcAEsDlY.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EiOVFR_XcAEsDlY.jpg",
+					"url": "https://t.co/VwlMWHQW4r",
+					"display_url": "pic.twitter.com/VwlMWHQW4r",
+					"expanded_url": "https://twitter.com/jdbence/status/1307052273126838273/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"small": {
+							"w": 300,
+							"h": 300,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 300,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 300,
+							"h": 300,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EiOVFR_XcAEsDlY.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I'm an archivist   •  #ATLUTD #SaintsFC #s04  •  my inner soundtrack is just buyout",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 694,
+			"friends_count": 920,
+			"listed_count": 30,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 4080,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3668,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1307051932251545600,
+		"quoted_status_id_str": "1307051932251545600",
+		"quoted_status_permalink": {
+			"url": "https://t.co/QzJrXJ9a8s",
+			"expanded": "https://twitter.com/s04_us/status/1307051932251545600",
+			"display": "twitter.com/s04_us/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Sep 18 20:20:39 +0000 2020",
+			"id": 1307051932251545600,
+			"id_str": "1307051932251545600",
+			"full_text": "It's over.\n\n⏱️ FT | #FCBS04 8-0 | #SchalkeUS🇺🇸 https://t.co/MuS6skcKVf",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				46
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "FCBS04",
+						"indices": [
+							20,
+							27
+						]
+					},
+					{
+						"text": "SchalkeUS",
+						"indices": [
+							34,
+							44
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1307051651870740500,
+						"id_str": "1307051651870740480",
+						"indices": [
+							47,
+							70
+						],
+						"media_url": "http://pbs.twimg.com/media/EiOUjWTX0AAfM_G.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EiOUjWTX0AAfM_G.jpg",
+						"url": "https://t.co/MuS6skcKVf",
+						"display_url": "pic.twitter.com/MuS6skcKVf",
+						"expanded_url": "https://twitter.com/s04_us/status/1307051932251545600/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 678,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1084,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 384,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1307051651870740500,
+						"id_str": "1307051651870740480",
+						"indices": [
+							47,
+							70
+						],
+						"media_url": "http://pbs.twimg.com/media/EiOUjWTX0AAfM_G.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EiOUjWTX0AAfM_G.jpg",
+						"url": "https://t.co/MuS6skcKVf",
+						"display_url": "pic.twitter.com/MuS6skcKVf",
+						"expanded_url": "https://twitter.com/s04_us/status/1307051932251545600/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 678,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1084,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 384,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3432500031,
+				"id_str": "3432500031",
+				"name": "Schalke 04 USA🇺🇸",
+				"screen_name": "s04_us",
+				"location": "Gelsenkirchen",
+				"description": "Welcome to the official #SchalkeUS🇺🇸 Twitter account! See also the other #S04 accounts: Deutsch @s04 | English @s04_en | 日本語: @s04_jp | Español @s04_es",
+				"url": "https://t.co/0FWdxDCGsd",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/0FWdxDCGsd",
+								"expanded_url": "http://www.schalke04.com",
+								"display_url": "schalke04.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 20194,
+				"friends_count": 403,
+				"listed_count": 155,
+				"created_at": "Thu Aug 20 08:26:39 +0000 2015",
+				"favourites_count": 2015,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 21380,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1210495371882958849/aiATLbRT_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210495371882958849/aiATLbRT_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3432500031/1600471076",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 40,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Not how I wanted to kickoff my Friday https://t.co/QzJrXJ9a8s https://t.co/VwlMWHQW4r"
+	},
+	{
 		"created_at": "Fri Sep 11 15:09:47 +0000 2020",
 		"id": 1304436987378761700,
 		"id_str": "1304436987378761729",
