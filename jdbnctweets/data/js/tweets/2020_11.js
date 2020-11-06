72567b88b080fbc8edd6796c97ca35ab7f43ef1a
John Bence <johndbence@gmail.com>
2020-11-05 22:19:37 -0500

    Added 1 tweets

--- jdbnctweets/data/js/tweets/2020_11.js
+++ jdbnctweets/data/js/tweets/2020_11.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Thu Nov 05 13:51:54 +0000 2020",
+		"id": 1324348718666129400,
+		"id_str": "1324348718666129409",
+		"full_text": "That’s my neighborhood. Seems right for 2020. https://t.co/Rc9UyZm1A6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			45
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Rc9UyZm1A6",
+					"expanded_url": "https://twitter.com/ajc/status/1324339380090212352",
+					"display_url": "twitter.com/ajc/status/132…",
+					"indices": [
+						46,
+						69
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I'm an archivist   •   #ATLUTD #SaintsFC #s04  •   my inner soundtrack is just buyout on repeat",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 703,
+			"friends_count": 936,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 4274,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3687,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1324339380090212400,
+		"quoted_status_id_str": "1324339380090212352",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Rc9UyZm1A6",
+			"expanded": "https://twitter.com/ajc/status/1324339380090212352",
+			"display": "twitter.com/ajc/status/132…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Nov 05 13:14:47 +0000 2020",
+			"id": 1324339380090212400,
+			"id_str": "1324339380090212352",
+			"full_text": "Garbage truck catches fire, dumps flaming load in Virginia-Highland\nhttps://t.co/6ePCGQfw4V",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				91
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/6ePCGQfw4V",
+						"expanded_url": "http://ajc.com/news/garbage-truck-catches-fire-dumps-flaming-load-in-virginia-highland/SOU2JIRYWVAELI7EPXXDJXNHKA/",
+						"display_url": "ajc.com/news/garbage-t…",
+						"indices": [
+							68,
+							91
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 4170491,
+				"id_str": "4170491",
+				"name": "AJC",
+				"screen_name": "ajc",
+				"location": "Atlanta, GA",
+				"description": "Atlanta's best source for news. You can reach our customer service team here: 404-522-4141, customercare@ajc.com",
+				"url": "https://t.co/jvkZJ5B2LG",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/jvkZJ5B2LG",
+								"expanded_url": "http://www.ajc.com",
+								"display_url": "ajc.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1057599,
+				"friends_count": 54074,
+				"listed_count": 5129,
+				"created_at": "Wed Apr 11 12:50:15 +0000 2007",
+				"favourites_count": 1395,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 290478,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "00539B",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1205233781319311361/y6T5pwyP_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1205233781319311361/y6T5pwyP_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/4170491/1596209593",
+				"profile_link_color": "00539B",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "F0F5ED",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 13,
+			"favorite_count": 59,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "That’s my neighborhood. Seems right for 2020. https://t.co/Rc9UyZm1A6"
+	},
+	{
 		"created_at": "Tue Nov 03 23:25:38 +0000 2020",
 		"id": 1323768328880558000,
 		"id_str": "1323768328880558085",
