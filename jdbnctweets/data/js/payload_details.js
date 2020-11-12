970b5e0bdd428447371a9375e63dbbeac8bc0fea
John Bence <johndbence@gmail.com>
2020-11-11 22:19:37 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3792,
-	"created_at": "Sat Nov 07 03:19:35 +0000 2020",
+	"tweets": 3793,
+	"created_at": "Thu Nov 12 03:19:35 +0000 2020",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2020_11.js
+++ jdbnctweets/data/js/tweets/2020_11.js
@@ -1,6 +1,98 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Thu Nov 12 00:56:25 +0000 2020",
+		"id": 1326667783929798700,
+		"id_str": "1326690275822014467",
+		"full_text": "@FelipeCar 🤣🤣🤣🤣🤣",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			16
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "FelipeCar",
+					"name": "Felipe Cárdenas",
+					"id": 17288797,
+					"id_str": "17288797",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1326658444422733800,
+		"in_reply_to_status_id_str": "1326658444422733824",
+		"in_reply_to_user_id": 17288797,
+		"in_reply_to_user_id_str": "17288797",
+		"in_reply_to_screen_name": "FelipeCar",
+		"user": {
+			"id": 1052913308146253800,
+			"id_str": "1052913308146253824",
+			"name": "Leandro Gonzalez Pirez",
+			"screen_name": "pirez_leandro",
+			"location": "Atlanta, GA",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 633,
+			"friends_count": 55,
+			"listed_count": 3,
+			"created_at": "Thu Oct 18 13:24:29 +0000 2018",
+			"favourites_count": 273,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 64,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1168727469454778368/5nXTXIEQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1168727469454778368/5nXTXIEQ_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 40,
+		"favorite_count": 393,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "und",
+		"text": "@FelipeCar 🤣🤣🤣🤣🤣"
+	},
+	{
 		"created_at": "Sat Nov 07 02:34:21 +0000 2020",
 		"id": 1324902984875204600,
 		"id_str": "1324902984875204608",
