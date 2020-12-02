49e1cac2795899cb1bf357b85076902fc0a546d6
John Bence <johndbence@gmail.com>
2020-12-01 22:19:37 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3795,
-	"created_at": "Fri Nov 27 03:19:35 +0000 2020",
+	"tweets": 3796,
+	"created_at": "Wed Dec 02 03:19:35 +0000 2020",
 	"lang": "en"
 }
---
+++ jdbnctweets/data/js/tweets/2020_12.js
@@ -0,0 +1,183 @@
+Grailbird.data.tweets_2020_12 = 
+[
+	{
+		"created_at": "Tue Dec 01 18:38:13 +0000 2020",
+		"id": 1333842141328658400,
+		"id_str": "1333842857581535232",
+		"full_text": "🎅🏻GIVEAWAY!🎅🏻\n\nWant to win this hand signed photo of Ralph? Follow the steps below 👇\n\n1. Follow @TheSaintCentre \n2. Retweet this tweet\n\nWinner chosen at random 04/12/20 \n\nGood luck! #saintsfc https://t.co/sYHjNkwFli",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			191
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "saintsfc",
+					"indices": [
+						182,
+						191
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TheSaintCentre",
+					"name": "The Saint Centre",
+					"id": 1073738569611702300,
+					"id_str": "1073738569611702272",
+					"indices": [
+						96,
+						111
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1333842136123445200,
+					"id_str": "1333842136123445249",
+					"indices": [
+						192,
+						215
+					],
+					"media_url": "http://pbs.twimg.com/media/EoLCWxYWEAE6utl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoLCWxYWEAE6utl.jpg",
+					"url": "https://t.co/sYHjNkwFli",
+					"display_url": "pic.twitter.com/sYHjNkwFli",
+					"expanded_url": "https://twitter.com/TheSaintCentre/status/1333842141328658434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 509,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 620,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 828,
+							"h": 620,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1333842136123445200,
+					"id_str": "1333842136123445249",
+					"indices": [
+						192,
+						215
+					],
+					"media_url": "http://pbs.twimg.com/media/EoLCWxYWEAE6utl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoLCWxYWEAE6utl.jpg",
+					"url": "https://t.co/sYHjNkwFli",
+					"display_url": "pic.twitter.com/sYHjNkwFli",
+					"expanded_url": "https://twitter.com/TheSaintCentre/status/1333842141328658434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 509,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 620,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 828,
+							"h": 620,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1073738569611702300,
+			"id_str": "1073738569611702272",
+			"name": "The Saint Centre",
+			"screen_name": "TheSaintCentre",
+			"location": "Southampton, England",
+			"description": "We are dedicated to bring you all the lastest #saintsfc news! Find us on Instagram @thesaintcentre",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1973,
+			"friends_count": 383,
+			"listed_count": 11,
+			"created_at": "Sat Dec 15 00:36:38 +0000 2018",
+			"favourites_count": 2511,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2941,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1083471149751848966/uQ7Mdx4J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1083471149751848966/uQ7Mdx4J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1073738569611702272/1593967777",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 16,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🎅🏻GIVEAWAY!🎅🏻\n\nWant to win this hand signed photo of Ralph? Follow the steps below 👇\n\n1. Follow @TheSaintCentre \n2. Retweet this tweet\n\nWinner chosen at random 04/12/20 \n\nGood luck! #saintsfc https://t.co/sYHjNkwFli"
+	}
+]
