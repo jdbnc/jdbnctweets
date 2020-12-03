c1e66ddd6fe43346b514adab076d22ba98e0dd68
John Bence <johndbence@gmail.com>
2020-12-02 22:19:37 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3796,
-	"created_at": "Wed Dec 02 03:19:35 +0000 2020",
+	"tweets": 3797,
+	"created_at": "Thu Dec 03 03:19:35 +0000 2020",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2020_12.js
+++ jdbnctweets/data/js/tweets/2020_12.js
@@ -1,6 +1,300 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Wed Dec 02 17:57:34 +0000 2020",
+		"id": 1334195015015424000,
+		"id_str": "1334195015015424002",
+		"full_text": "👀 https://t.co/5jjat9yW1l",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			1
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5jjat9yW1l",
+					"expanded_url": "https://twitter.com/CityofAtlanta/status/1334174796045807617",
+					"display_url": "twitter.com/CityofAtlanta/…",
+					"indices": [
+						2,
+						25
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I'm an archivist   •   #ATLUTD #SaintsFC #s04  •   my inner soundtrack is just buyout on repeat",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 705,
+			"friends_count": 938,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 4402,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3693,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1334174796045807600,
+		"quoted_status_id_str": "1334174796045807617",
+		"quoted_status_permalink": {
+			"url": "https://t.co/5jjat9yW1l",
+			"expanded": "https://twitter.com/CityofAtlanta/status/1334174796045807617",
+			"display": "twitter.com/CityofAtlanta/…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Dec 02 16:37:13 +0000 2020",
+			"id": 1334174796045807600,
+			"id_str": "1334174796045807617",
+			"full_text": "Thank you to the @FIFAWorldCup Organizing Committee for considering Atlanta as a Host City for the 2026 competition! Learn more: https://t.co/6NQWeGnuYS https://t.co/Y6rE4cFHU8",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				152
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "FIFAWorldCup",
+						"name": "FIFA World Cup",
+						"id": 138372303,
+						"id_str": "138372303",
+						"indices": [
+							17,
+							30
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/6NQWeGnuYS",
+						"expanded_url": "https://www.atlantaga.gov/Home/Components/News/News/13501/1338",
+						"display_url": "atlantaga.gov/Home/Component…",
+						"indices": [
+							129,
+							152
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1334174783383154700,
+						"id_str": "1334174783383154696",
+						"indices": [
+							153,
+							176
+						],
+						"media_url": "http://pbs.twimg.com/media/EoPw5Y_WMAgq2ZL.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EoPw5Y_WMAgq2ZL.jpg",
+						"url": "https://t.co/Y6rE4cFHU8",
+						"display_url": "pic.twitter.com/Y6rE4cFHU8",
+						"expanded_url": "https://twitter.com/CityofAtlanta/status/1334174796045807617/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1024,
+								"h": 580,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 580,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 385,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1334174783383154700,
+						"id_str": "1334174783383154696",
+						"indices": [
+							153,
+							176
+						],
+						"media_url": "http://pbs.twimg.com/media/EoPw5Y_WMAgq2ZL.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EoPw5Y_WMAgq2ZL.jpg",
+						"url": "https://t.co/Y6rE4cFHU8",
+						"display_url": "pic.twitter.com/Y6rE4cFHU8",
+						"expanded_url": "https://twitter.com/CityofAtlanta/status/1334174796045807617/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1024,
+								"h": 580,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 580,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 385,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 21336038,
+				"id_str": "21336038",
+				"name": "City of Atlanta, GA",
+				"screen_name": "CityofAtlanta",
+				"location": "Atlanta, GA",
+				"description": "The Official Twitter account for City of Atlanta government.",
+				"url": "https://t.co/jeQzJSgP2j",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/jeQzJSgP2j",
+								"expanded_url": "https://www.atlantaga.gov/",
+								"display_url": "atlantaga.gov",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 603281,
+				"friends_count": 815,
+				"listed_count": 1602,
+				"created_at": "Thu Feb 19 20:34:40 +0000 2009",
+				"favourites_count": 9874,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 14782,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "311BF3",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1118589600539336704/-dRVqqg__normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1118589600539336704/-dRVqqg__normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/21336038/1559703920",
+				"profile_link_color": "E6CB51",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "58A8F3",
+				"profile_text_color": "311BF3",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 81,
+			"favorite_count": 377,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "👀 https://t.co/5jjat9yW1l"
+	},
+	{
 		"created_at": "Tue Dec 01 18:38:13 +0000 2020",
 		"id": 1333842141328658400,
 		"id_str": "1333842857581535232",
