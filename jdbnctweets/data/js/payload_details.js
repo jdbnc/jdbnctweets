16d54161cf720502b790e86b9dd7c2689611d441
John Bence <johndbence@gmail.com>
2021-01-04 22:19:36 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3806,
-	"created_at": "Thu Dec 31 03:19:35 +0000 2020",
+	"tweets": 3807,
+	"created_at": "Tue Jan 05 03:19:35 +0000 2021",
 	"lang": "en"
 }
---
+++ jdbnctweets/data/js/tweets/2021_01.js
@@ -0,0 +1,302 @@
+Grailbird.data.tweets_2021_01 = 
+[
+	{
+		"created_at": "Mon Jan 04 22:00:45 +0000 2021",
+		"id": 1346215013376290800,
+		"id_str": "1346215013376290817",
+		"full_text": "🥺❤️💪🏻 https://t.co/alzwVfjoZW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			5
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/alzwVfjoZW",
+					"expanded_url": "https://twitter.com/simonrichards/status/1346214114188734473",
+					"display_url": "twitter.com/simonrichards/…",
+					"indices": [
+						6,
+						29
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I'm an archivist   •   #ATLUTD #SaintsFC #s04  •   my inner soundtrack is just buyout on repeat",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 705,
+			"friends_count": 947,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 4507,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3705,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1346214114188734500,
+		"quoted_status_id_str": "1346214114188734473",
+		"quoted_status_permalink": {
+			"url": "https://t.co/alzwVfjoZW",
+			"expanded": "https://twitter.com/simonrichards/status/1346214114188734473",
+			"display": "twitter.com/simonrichards/…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jan 04 21:57:10 +0000 2021",
+			"id": 1346214114188734500,
+			"id_str": "1346214114188734473",
+			"full_text": "I’m not crying, you’re crying. I fucking love this man #saintsfc https://t.co/vcOVMLQQrF",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				64
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "saintsfc",
+						"indices": [
+							55,
+							64
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1346214029832945700,
+						"id_str": "1346214029832945668",
+						"indices": [
+							65,
+							88
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1346214029832945668/pu/img/VVxSnfNvhKMWsB09.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1346214029832945668/pu/img/VVxSnfNvhKMWsB09.jpg",
+						"url": "https://t.co/vcOVMLQQrF",
+						"display_url": "pic.twitter.com/vcOVMLQQrF",
+						"expanded_url": "https://twitter.com/simonrichards/status/1346214114188734473/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 555,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 315,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 592,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1346214029832945700,
+						"id_str": "1346214029832945668",
+						"indices": [
+							65,
+							88
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1346214029832945668/pu/img/VVxSnfNvhKMWsB09.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1346214029832945668/pu/img/VVxSnfNvhKMWsB09.jpg",
+						"url": "https://t.co/vcOVMLQQrF",
+						"display_url": "pic.twitter.com/vcOVMLQQrF",
+						"expanded_url": "https://twitter.com/simonrichards/status/1346214114188734473/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 555,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 315,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 592,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								80,
+								37
+							],
+							"duration_millis": 18685,
+							"variants": [
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1346214029832945668/pu/vid/582x270/kBF9dsyHJHOMNNF-.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1346214029832945668/pu/pl/NWF9Mca2r1MKIMU5.m3u8?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1346214029832945668/pu/vid/1280x592/XZA2ux3ntbh8oZte.mp4?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1346214029832945668/pu/vid/778x360/SGlH-fVtcKo0NGrj.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 19860872,
+				"id_str": "19860872",
+				"name": "Simon Richards",
+				"screen_name": "simonrichards",
+				"location": "Southampton",
+				"description": "Makes stuff for fast cars. Husband, Father. Carbon genius for #RedBull, #MclarenF1, #WilliamsF1. #saintsfc nut, Live music enthusiast.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1297,
+				"friends_count": 261,
+				"listed_count": 15,
+				"created_at": "Sun Feb 01 14:48:37 +0000 2009",
+				"favourites_count": 6152,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5626,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1312693218308489216/Wo0ze-k4_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1312693218308489216/Wo0ze-k4_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/19860872/1545949607",
+				"profile_link_color": "2FC2EF",
+				"profile_sidebar_border_color": "181A1E",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 81,
+			"favorite_count": 584,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "🥺❤️💪🏻 https://t.co/alzwVfjoZW"
+	}
+]
