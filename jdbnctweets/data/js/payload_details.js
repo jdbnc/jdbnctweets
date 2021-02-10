95fca011bdd5e1400f31e895fc7372b59dfc2528
John Bence <johndbence@gmail.com>
2021-02-09 22:19:36 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3818,
-	"created_at": "Wed Feb 03 03:19:42 +0000 2021",
+	"tweets": 3819,
+	"created_at": "Wed Feb 10 03:19:35 +0000 2021",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2021_02.js
+++ jdbnctweets/data/js/tweets/2021_02.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Tue Feb 09 18:59:55 +0000 2021",
+		"id": 1359215468284887000,
+		"id_str": "1359215468284887040",
+		"full_text": "@ATLPublicWorks Will you be providing additional bins to hold recycling for the extra week? We've got ~20 residents and are full every week.",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ATLPublicWorks",
+					"name": "ATLPublicWorks",
+					"id": 2911318678,
+					"id_str": "2911318678",
+					"indices": [
+						0,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1359214548125892600,
+		"in_reply_to_status_id_str": "1359214548125892610",
+		"in_reply_to_user_id": 2911318678,
+		"in_reply_to_user_id_str": "2911318678",
+		"in_reply_to_screen_name": "ATLPublicWorks",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I'm an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 708,
+			"friends_count": 982,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 4758,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3717,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@ATLPublicWorks Will you be providing additional bins to hold recycling for the extra week? We've got ~20 residents and are full every week."
+	},
+	{
 		"created_at": "Tue Feb 02 22:54:26 +0000 2021",
 		"id": 1356737773064061000,
 		"id_str": "1356737773064060940",
