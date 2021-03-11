a50f2dc574941d86c48f23b3691b2e447c7b62f9
John Bence <johndbence@gmail.com>
2021-03-10 22:19:35 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3823,
-	"created_at": "Fri Mar 05 03:19:34 +0000 2021",
+	"tweets": 3824,
+	"created_at": "Thu Mar 11 03:19:34 +0000 2021",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2021_03.js
+++ jdbnctweets/data/js/tweets/2021_03.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Wed Mar 10 21:51:58 +0000 2021",
+		"id": 1369768016087498800,
+		"id_str": "1369768016087498752",
+		"full_text": "Spring has arrived in Atlanta 🌸 https://t.co/UsqwcJKVgY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			31
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1369768009649295400,
+					"id_str": "1369768009649295368",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/EwJkv1fXEAgel-E.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EwJkv1fXEAgel-E.jpg",
+					"url": "https://t.co/UsqwcJKVgY",
+					"display_url": "pic.twitter.com/UsqwcJKVgY",
+					"expanded_url": "https://twitter.com/jdbence/status/1369768016087498752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1369768009649295400,
+					"id_str": "1369768009649295368",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/EwJkv1fXEAgel-E.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EwJkv1fXEAgel-E.jpg",
+					"url": "https://t.co/UsqwcJKVgY",
+					"display_url": "pic.twitter.com/UsqwcJKVgY",
+					"expanded_url": "https://twitter.com/jdbence/status/1369768016087498752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I'm an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 708,
+			"friends_count": 997,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 4853,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3720,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1269236846837587968/FuxvI9Nc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Spring has arrived in Atlanta 🌸 https://t.co/UsqwcJKVgY"
+	},
+	{
 		"created_at": "Thu Mar 04 14:47:13 +0000 2021",
 		"id": 1367486796586766300,
 		"id_str": "1367486796586766343",
