a48002d178696beff1a192566f8bd28832b1c914
John Bence <johndbence@gmail.com>
2021-09-11 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3869,
-	"created_at": "Thu Sep 09 03:19:35 +0000 2021",
+	"tweets": 3870,
+	"created_at": "Sun Sep 12 03:19:35 +0000 2021",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2021_09.js
+++ jdbnctweets/data/js/tweets/2021_09.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sat Sep 11 11:54:30 +0000 2021",
+		"id": 1436659414375833600,
+		"id_str": "1436659414375833601",
+		"full_text": "@Adi28326551 Appreciate the tip! 💪🏻😇",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Adi28326551",
+					"name": "Adi",
+					"id": 1367787487058333700,
+					"id_str": "1367787487058333699",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1436629244944265200,
+		"in_reply_to_status_id_str": "1436629244944265218",
+		"in_reply_to_user_id": 1367787487058333700,
+		"in_reply_to_user_id_str": "1367787487058333699",
+		"in_reply_to_screen_name": "Adi28326551",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I'm an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 734,
+			"friends_count": 1043,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 5544,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3761,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Adi28326551 Appreciate the tip! 💪🏻😇"
+	},
+	{
 		"created_at": "Wed Sep 08 15:17:34 +0000 2021",
 		"id": 1435623350630555600,
 		"id_str": "1435623350630555654",
