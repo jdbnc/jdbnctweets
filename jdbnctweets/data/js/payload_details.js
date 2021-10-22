b89b3dcb8f8e8e959accf1fc8106697bfffa46dd
John Bence <johndbence@gmail.com>
2021-10-21 23:19:38 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3879,
-	"created_at": "Thu Oct 21 03:19:35 +0000 2021",
+	"tweets": 3880,
+	"created_at": "Fri Oct 22 03:19:36 +0000 2021",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2021_10.js
+++ jdbnctweets/data/js/tweets/2021_10.js
@@ -1,6 +1,130 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Thu Oct 21 14:00:03 +0000 2021",
+		"id": 1451186524117094400,
+		"id_str": "1451186524117094407",
+		"full_text": "@HappiestofJoes @SouthamptonFC @SantaUnited @randomthgts1 Sadly, no. Bummer to miss the Great Atlanta Saints gathering!",
+		"truncated": false,
+		"display_text_range": [
+			58,
+			119
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "HappiestofJoes",
+					"name": "Joe Johnstone",
+					"id": 713249509,
+					"id_str": "713249509",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "SouthamptonFC",
+					"name": "Southampton FC",
+					"id": 46143892,
+					"id_str": "46143892",
+					"indices": [
+						16,
+						30
+					]
+				},
+				{
+					"screen_name": "SantaUnited",
+					"name": "Santa🔴⚫️🎅🏼⚫️🔴",
+					"id": 836730235315966000,
+					"id_str": "836730235315965952",
+					"indices": [
+						31,
+						43
+					]
+				},
+				{
+					"screen_name": "randomthgts1",
+					"name": "Will Payman 🏆 🏆 ⚫️🔴",
+					"id": 3033263844,
+					"id_str": "3033263844",
+					"indices": [
+						44,
+						57
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1451153617222873000,
+		"in_reply_to_status_id_str": "1451153617222873089",
+		"in_reply_to_user_id": 713249509,
+		"in_reply_to_user_id_str": "713249509",
+		"in_reply_to_screen_name": "HappiestofJoes",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I’m an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 736,
+			"friends_count": 1046,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 5654,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3770,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@HappiestofJoes @SouthamptonFC @SantaUnited @randomthgts1 Sadly, no. Bummer to miss the Great Atlanta Saints gathering!"
+	},
+	{
 		"created_at": "Thu Oct 21 01:25:46 +0000 2021",
 		"id": 1450996701007958000,
 		"id_str": "1450996701007958016",
