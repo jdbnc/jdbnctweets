4831f05002988d02bd3f5c038c2426c6e0aa672c
John Bence <johndbence@gmail.com>
2021-12-16 22:19:35 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3898,
-	"created_at": "Thu Dec 16 03:19:35 +0000 2021",
+	"tweets": 3899,
+	"created_at": "Fri Dec 17 03:19:34 +0000 2021",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2021_12.js
+++ jdbnctweets/data/js/tweets/2021_12.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Thu Dec 16 12:50:36 +0000 2021",
+		"id": 1471416571365056500,
+		"id_str": "1471462767391559680",
+		"full_text": "Damn me, but all things are queer",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			33
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://twitter.com/MobyDickatSea\" rel=\"nofollow\">Moby Dick at Sea</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1482779976,
+			"id_str": "1482779976",
+			"name": "Moby Dick",
+			"screen_name": "MobyDickatSea",
+			"location": "The South Seas",
+			"description": "Random lines from Moby Dick. Conceived, coded, and curated by @samplereality. Companion bot @FindMobyDick posts citations.",
+			"url": "https://t.co/ZSPUkwbUo7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZSPUkwbUo7",
+							"expanded_url": "https://ko-fi.com/mobydickatsea",
+							"display_url": "ko-fi.com/mobydickatsea",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 93428,
+			"friends_count": 1,
+			"listed_count": 799,
+			"created_at": "Tue Jun 04 17:23:46 +0000 2013",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 29645,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "3289B5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3755781902/9b47b7e223799bb523c7628e00b411c4_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3755781902/9b47b7e223799bb523c7628e00b411c4_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1482779976/1638113751",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 193,
+		"favorite_count": 832,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Damn me, but all things are queer"
+	},
+	{
 		"created_at": "Wed Dec 15 13:50:40 +0000 2021",
 		"id": 1471115495021633500,
 		"id_str": "1471115495021633537",
