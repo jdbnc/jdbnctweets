cc0cc8270f5354e64004ed89b0b76883d86ba600
John Bence <johndbence@gmail.com>
2021-12-26 22:19:35 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3900,
-	"created_at": "Sun Dec 19 03:19:34 +0000 2021",
+	"tweets": 3901,
+	"created_at": "Mon Dec 27 03:19:34 +0000 2021",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2021_12.js
+++ jdbnctweets/data/js/tweets/2021_12.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Sun Dec 26 14:43:16 +0000 2021",
+		"id": 1475114997265096700,
+		"id_str": "1475114997265096704",
+		"full_text": "@SantaUnited @SouthhamptonFC Hell yeah!",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			39
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SantaUnited",
+					"name": "Santa🔴⚫️🎅🏼⚫️🔴",
+					"id": 836730235315966000,
+					"id_str": "836730235315965952",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "SouthhamptonFC",
+					"name": "Southhampton",
+					"id": 448897329,
+					"id_str": "448897329",
+					"indices": [
+						13,
+						28
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1475112181062680600,
+		"in_reply_to_status_id_str": "1475112181062680580",
+		"in_reply_to_user_id": 836730235315966000,
+		"in_reply_to_user_id_str": "836730235315965952",
+		"in_reply_to_screen_name": "SantaUnited",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I’m an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 743,
+			"friends_count": 1058,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 5987,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3791,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@SantaUnited @SouthhamptonFC Hell yeah!"
+	},
+	{
 		"created_at": "Sat Dec 18 19:04:08 +0000 2021",
 		"id": 1472281544861667300,
 		"id_str": "1472281544861667330",
