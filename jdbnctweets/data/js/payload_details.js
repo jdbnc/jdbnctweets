33b01f49e328e8389c66140118491d15091e687c
John Bence <johndbence@gmail.com>
2022-02-03 22:19:35 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3906,
-	"created_at": "Thu Jan 27 03:19:34 +0000 2022",
+	"tweets": 3907,
+	"created_at": "Fri Feb 04 03:19:34 +0000 2022",
 	"lang": "en"
 }
---
+++ jdbnctweets/data/js/tweets/2022_02.js
@@ -0,0 +1,352 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Thu Feb 03 17:09:04 +0000 2022",
+		"id": 1489284818030497800,
+		"id_str": "1489284818030497798",
+		"full_text": "🚨 Happening now 🚨 https://t.co/isIGr8ExDN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			17
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/isIGr8ExDN",
+					"expanded_url": "https://twitter.com/EmoryLibraries/status/1489271390305984518",
+					"display_url": "twitter.com/EmoryLibraries…",
+					"indices": [
+						18,
+						41
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I’m an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 741,
+			"friends_count": 1071,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 6140,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3800,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1489271390305984500,
+		"quoted_status_id_str": "1489271390305984518",
+		"quoted_status_permalink": {
+			"url": "https://t.co/isIGr8ExDN",
+			"expanded": "https://twitter.com/EmoryLibraries/status/1489271390305984518",
+			"display": "twitter.com/EmoryLibraries…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Feb 03 16:15:43 +0000 2022",
+			"id": 1489271390305984500,
+			"id_str": "1489271390305984518",
+			"full_text": "TODAY Feb 3 online at 12p Jim Crow in the Asylum: Contesting Custody &amp; Care in Southern Psychiatric Hospitals after WWII with Kylie Smith PhD @EmoryHistory @EmoryNursing  https://t.co/hascbakjYM #blackhistorymonth #africanamerican #archives #mentalhealthcare #civilrights #nlm https://t.co/tP0qCUxA9F",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "blackhistorymonth",
+						"indices": [
+							199,
+							217
+						]
+					},
+					{
+						"text": "africanamerican",
+						"indices": [
+							218,
+							234
+						]
+					},
+					{
+						"text": "archives",
+						"indices": [
+							235,
+							244
+						]
+					},
+					{
+						"text": "mentalhealthcare",
+						"indices": [
+							245,
+							262
+						]
+					},
+					{
+						"text": "civilrights",
+						"indices": [
+							263,
+							275
+						]
+					},
+					{
+						"text": "nlm",
+						"indices": [
+							276,
+							280
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "EmoryHistory",
+						"name": "Emory History",
+						"id": 1338939976529088500,
+						"id_str": "1338939976529088514",
+						"indices": [
+							146,
+							159
+						]
+					},
+					{
+						"screen_name": "EmoryNursing",
+						"name": "Emory Nursing",
+						"id": 488040421,
+						"id_str": "488040421",
+						"indices": [
+							160,
+							173
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/hascbakjYM",
+						"expanded_url": "https://youtu.be/vK1ZKvJWp3M",
+						"display_url": "youtu.be/vK1ZKvJWp3M",
+						"indices": [
+							175,
+							198
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1489270903691255800,
+						"id_str": "1489270903691255809",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/FKrz_xTXoAEc0zz.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FKrz_xTXoAEc0zz.jpg",
+						"url": "https://t.co/tP0qCUxA9F",
+						"display_url": "pic.twitter.com/tP0qCUxA9F",
+						"expanded_url": "https://twitter.com/EmoryLibraries/status/1489271390305984518/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 800,
+								"h": 400,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 800,
+								"h": 400,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1489270903691255800,
+						"id_str": "1489270903691255809",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/FKrz_xTXoAEc0zz.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FKrz_xTXoAEc0zz.jpg",
+						"url": "https://t.co/tP0qCUxA9F",
+						"display_url": "pic.twitter.com/tP0qCUxA9F",
+						"expanded_url": "https://twitter.com/EmoryLibraries/status/1489271390305984518/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 800,
+								"h": 400,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 800,
+								"h": 400,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 150683800,
+				"id_str": "150683800",
+				"name": "Emory Libraries",
+				"screen_name": "EmoryLibraries",
+				"location": "Atlanta, Georgia",
+				"description": "Libraries include the Woodruff Library; Rose Manuscript, Archives, & Rare Book Library; Woodruff Health Sciences Center Library, & Oxford College Library",
+				"url": "https://t.co/YCVKrPzKFj",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/YCVKrPzKFj",
+								"expanded_url": "http://libraries.emory.edu/",
+								"display_url": "libraries.emory.edu",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2344,
+				"friends_count": 951,
+				"listed_count": 105,
+				"created_at": "Tue Jun 01 15:10:35 +0000 2010",
+				"favourites_count": 3321,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 3949,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/829775118226309121/fWNKxMnB_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/829775118226309121/fWNKxMnB_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/150683800/1417540597",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 10,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🚨 Happening now 🚨 https://t.co/isIGr8ExDN"
+	}
+]
