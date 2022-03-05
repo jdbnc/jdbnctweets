643e5fa231df1baee87fc8abce51715e957e4562
John Bence <johndbence@gmail.com>
2022-03-04 22:19:36 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3919,
-	"created_at": "Fri Mar 04 03:19:35 +0000 2022",
+	"tweets": 3920,
+	"created_at": "Sat Mar 05 03:19:35 +0000 2022",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2022_03.js
+++ jdbnctweets/data/js/tweets/2022_03.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Fri Mar 04 16:44:48 +0000 2022",
+		"id": 1499787956730667000,
+		"id_str": "1499787956730667008",
+		"full_text": "@meaghanoriordan @theblacktux Well, it’s tomorrow so I’m basically up the creek 😭",
+		"truncated": false,
+		"display_text_range": [
+			30,
+			81
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "meaghanoriordan",
+					"name": "Meaghan Thee Archivist",
+					"id": 23823656,
+					"id_str": "23823656",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "theblacktux",
+					"name": "The Black Tux",
+					"id": 525021451,
+					"id_str": "525021451",
+					"indices": [
+						17,
+						29
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1499785842570768400,
+		"in_reply_to_status_id_str": "1499785842570768391",
+		"in_reply_to_user_id": 23823656,
+		"in_reply_to_user_id_str": "23823656",
+		"in_reply_to_screen_name": "meaghanoriordan",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I’m an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 741,
+			"friends_count": 1074,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 6331,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3813,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@meaghanoriordan @theblacktux Well, it’s tomorrow so I’m basically up the creek 😭"
+	},
+	{
 		"created_at": "Thu Mar 03 15:00:10 +0000 2022",
 		"id": 1499399236776988700,
 		"id_str": "1499399236776988672",
