0fcc84f22e878bae689acdce8ca9a8ad24dd03ac
John Bence <johndbence@gmail.com>
2022-03-30 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/tweets/2022_03.js
+++ jdbnctweets/data/js/tweets/2022_03.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Wed Mar 30 16:47:15 +0000 2022",
+		"id": 1509210605173018600,
+		"id_str": "1509210657232719876",
+		"full_text": "Thou art too damned jolly. Sail on.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://twitter.com/MobyDickatSea\" rel=\"nofollow\">Moby Dick at Sea</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1482779976,
+			"id_str": "1482779976",
+			"name": "Moby Dick",
+			"screen_name": "MobyDickatSea",
+			"location": "The South Seas",
+			"description": "Random lines from Moby Dick. Bot conceived, coded, and curated by Mark Sample. Companion bot @FindMobyDick posts citations.",
+			"url": "https://t.co/ZSPUkwbUo7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZSPUkwbUo7",
+							"expanded_url": "https://ko-fi.com/mobydickatsea",
+							"display_url": "ko-fi.com/mobydickatsea",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 96314,
+			"friends_count": 1,
+			"listed_count": 821,
+			"created_at": "Tue Jun 04 17:23:46 +0000 2013",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 30807,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "3289B5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3755781902/9b47b7e223799bb523c7628e00b411c4_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3755781902/9b47b7e223799bb523c7628e00b411c4_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1482779976/1638113751",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 144,
+		"favorite_count": 500,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Thou art too damned jolly. Sail on."
+	},
+	{
 		"created_at": "Tue Mar 29 15:55:46 +0000 2022",
 		"id": 1508835165883220000,
 		"id_str": "1508835315787735052",
