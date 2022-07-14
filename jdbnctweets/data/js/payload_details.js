65e725d2f5b16d9464d0756072db9a939f2790b8
John Bence <johndbence@gmail.com>
2022-07-13 23:19:35 -0400

    Added 2 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3951,
-	"created_at": "Tue Jul 12 03:19:34 +0000 2022",
+	"tweets": 3953,
+	"created_at": "Thu Jul 14 03:19:34 +0000 2022",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2022_07.js
+++ jdbnctweets/data/js/tweets/2022_07.js
@@ -1,6 +1,424 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Wed Jul 13 18:20:10 +0000 2022",
+		"id": 1547284765971202000,
+		"id_str": "1547284765971202048",
+		"full_text": "@kwittgens Thanks 🙃",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			19
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kwittgens",
+					"name": "the KatiE W ☀️",
+					"id": 224360311,
+					"id_str": "224360311",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1547284098355527700,
+		"in_reply_to_status_id_str": "1547284098355527680",
+		"in_reply_to_user_id": 224360311,
+		"in_reply_to_user_id_str": "224360311",
+		"in_reply_to_screen_name": "kwittgens",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I’m an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 733,
+			"friends_count": 1087,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 6692,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3844,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@kwittgens Thanks 🙃"
+	},
+	{
+		"created_at": "Wed Jul 13 17:56:28 +0000 2022",
+		"id": 1547278804397293600,
+		"id_str": "1547278804397293576",
+		"full_text": "I entered a contest to win two tickets to tonight’s game, WON, and this is how I feel about it #ATLUTD https://t.co/v8gax0cMog",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			102
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ATLUTD",
+					"indices": [
+						95,
+						102
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/v8gax0cMog",
+					"expanded_url": "https://twitter.com/kwittgens/status/1547264393079128064",
+					"display_url": "twitter.com/kwittgens/stat…",
+					"indices": [
+						103,
+						126
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I’m an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 733,
+			"friends_count": 1087,
+			"listed_count": 29,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 6692,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3844,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1547264393079128000,
+		"quoted_status_id_str": "1547264393079128064",
+		"quoted_status_permalink": {
+			"url": "https://t.co/v8gax0cMog",
+			"expanded": "https://twitter.com/kwittgens/status/1547264393079128064",
+			"display": "twitter.com/kwittgens/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jul 13 16:59:12 +0000 2022",
+			"id": 1547264393079128000,
+			"id_str": "1547264393079128064",
+			"full_text": "Hey, it’s another #ATLUTD gameday.. https://t.co/p3mEtlvei8",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				35
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "ATLUTD",
+						"indices": [
+							18,
+							25
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1547264387660058600,
+						"id_str": "1547264387660058624",
+						"indices": [
+							36,
+							59
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/FXj8w0gXEAAEaxF.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FXj8w0gXEAAEaxF.jpg",
+						"url": "https://t.co/p3mEtlvei8",
+						"display_url": "pic.twitter.com/p3mEtlvei8",
+						"expanded_url": "https://twitter.com/kwittgens/status/1547264393079128064/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 220,
+								"h": 220,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 220,
+								"h": 220,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 220,
+								"h": 220,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1547264387660058600,
+						"id_str": "1547264387660058624",
+						"indices": [
+							36,
+							59
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/FXj8w0gXEAAEaxF.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FXj8w0gXEAAEaxF.jpg",
+						"url": "https://t.co/p3mEtlvei8",
+						"display_url": "pic.twitter.com/p3mEtlvei8",
+						"expanded_url": "https://twitter.com/kwittgens/status/1547264393079128064/photo/1",
+						"type": "animated_gif",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 220,
+								"h": 220,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 220,
+								"h": 220,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 220,
+								"h": 220,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"variants": [
+								{
+									"bitrate": 0,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/tweet_video/FXj8w0gXEAAEaxF.mp4"
+								}
+							]
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 224360311,
+				"id_str": "224360311",
+				"name": "the KatiE W ☀️",
+				"screen_name": "kwittgens",
+				"location": "atlanta",
+				"description": "mostly tweets about sports and music",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 442,
+				"friends_count": 1117,
+				"listed_count": 2,
+				"created_at": "Wed Dec 08 20:11:35 +0000 2010",
+				"favourites_count": 32518,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 7515,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "BADFCD",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1094064696372666368/EBQwaA53_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1094064696372666368/EBQwaA53_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/224360311/1646061932",
+				"profile_link_color": "FF0000",
+				"profile_sidebar_border_color": "F2E195",
+				"profile_sidebar_fill_color": "FFF7CC",
+				"profile_text_color": "0C3E53",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "00c26afc77ee7aaa",
+				"url": "https://api.twitter.com/1.1/geo/id/00c26afc77ee7aaa.json",
+				"place_type": "city",
+				"name": "Brookhaven",
+				"full_name": "Brookhaven, GA",
+				"country_code": "US",
+				"country": "United States",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-84.348581,
+								33.825123
+							],
+							[
+								-84.28156,
+								33.825123
+							],
+							[
+								-84.28156,
+								33.923964
+							],
+							[
+								-84.348581,
+								33.923964
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 18,
+			"favorite_count": 207,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I entered a contest to win two tickets to tonight’s game, WON, and this is how I feel about it #ATLUTD https://t.co/v8gax0cMog"
+	},
+	{
 		"created_at": "Mon Jul 11 14:51:29 +0000 2022",
 		"id": 1546507473724514300,
 		"id_str": "1546507473724514304",
