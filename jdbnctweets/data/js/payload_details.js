e738e66195650dc22096e644d871cf7ce809be51
John Bence <johndbence@gmail.com>
2022-08-06 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3961,
-	"created_at": "Sat Aug 06 03:19:34 +0000 2022",
+	"tweets": 3962,
+	"created_at": "Sun Aug 07 03:19:34 +0000 2022",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2022_08.js
+++ jdbnctweets/data/js/tweets/2022_08.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sat Aug 06 12:56:11 +0000 2022",
+		"id": 1555900545117954000,
+		"id_str": "1555900545117954048",
+		"full_text": "@SantaUnited @PLinUSA Up The Saints!!!",
+		"truncated": false,
+		"display_text_range": [
+			22,
+			38
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SantaUnited",
+					"name": "Santa🔴⚫️🎅🏼⚫️🔴",
+					"id": 836730235315966000,
+					"id_str": "836730235315965952",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "PLinUSA",
+					"name": "Premier League USA",
+					"id": 822225946916884500,
+					"id_str": "822225946916884480",
+					"indices": [
+						13,
+						21
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1555899581199777800,
+		"in_reply_to_status_id_str": "1555899581199777792",
+		"in_reply_to_user_id": 836730235315966000,
+		"in_reply_to_user_id_str": "836730235315965952",
+		"in_reply_to_screen_name": "SantaUnited",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, I’m an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 734,
+			"friends_count": 1101,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 6769,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3847,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@SantaUnited @PLinUSA Up The Saints!!!"
+	},
+	{
 		"created_at": "Fri Aug 05 23:02:08 +0000 2022",
 		"id": 1555690650049314800,
 		"id_str": "1555690650049314817",
