0d55c36e24b391386c677093bb9c0b8020ae2b1c
John Bence <johndbence@gmail.com>
2022-08-14 23:19:35 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3962,
-	"created_at": "Sun Aug 07 03:19:34 +0000 2022",
+	"tweets": 3963,
+	"created_at": "Mon Aug 15 03:19:34 +0000 2022",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2022_08.js
+++ jdbnctweets/data/js/tweets/2022_08.js
@@ -1,6 +1,289 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Mon Aug 15 00:25:49 +0000 2022",
+		"id": 1558629357932884000,
+		"id_str": "1558973199764914176",
+		"full_text": "Dream Blunt rotation https://t.co/fbl0DRBAQ9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1558629347174465500,
+					"id_str": "1558629347174465536",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/FaFdIh7VQAALZYL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaFdIh7VQAALZYL.jpg",
+					"url": "https://t.co/fbl0DRBAQ9",
+					"display_url": "pic.twitter.com/fbl0DRBAQ9",
+					"expanded_url": "https://twitter.com/chrtucci/status/1558629357932883968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1558629347174465500,
+					"id_str": "1558629347174465536",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/FaFdIh7VQAALZYL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaFdIh7VQAALZYL.jpg",
+					"url": "https://t.co/fbl0DRBAQ9",
+					"display_url": "pic.twitter.com/fbl0DRBAQ9",
+					"expanded_url": "https://twitter.com/chrtucci/status/1558629357932883968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1558629347161804800,
+					"id_str": "1558629347161804800",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/FaFdIh4UEAAZOiA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaFdIh4UEAAZOiA.jpg",
+					"url": "https://t.co/fbl0DRBAQ9",
+					"display_url": "pic.twitter.com/fbl0DRBAQ9",
+					"expanded_url": "https://twitter.com/chrtucci/status/1558629357932883968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1558629347166105600,
+					"id_str": "1558629347166105600",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/FaFdIh5VsAAMLL3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaFdIh5VsAAMLL3.jpg",
+					"url": "https://t.co/fbl0DRBAQ9",
+					"display_url": "pic.twitter.com/fbl0DRBAQ9",
+					"expanded_url": "https://twitter.com/chrtucci/status/1558629357932883968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1558629347166019600,
+					"id_str": "1558629347166019584",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/FaFdIh5UYAAop-p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaFdIh5UYAAop-p.jpg",
+					"url": "https://t.co/fbl0DRBAQ9",
+					"display_url": "pic.twitter.com/fbl0DRBAQ9",
+					"expanded_url": "https://twitter.com/chrtucci/status/1558629357932883968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2262122917,
+			"id_str": "2262122917",
+			"name": "Christian Tucci",
+			"screen_name": "chrtucci",
+			"location": "Los Angeles, CA",
+			"description": "writer & artist 🏳️‍🌈 he/him 👻👻🔎 $chrtucci drawings are for sale, DM if interested",
+			"url": "https://t.co/UzYZzKBEk7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UzYZzKBEk7",
+							"expanded_url": "http://christian-tucci.com",
+							"display_url": "christian-tucci.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 38502,
+			"friends_count": 1766,
+			"listed_count": 60,
+			"created_at": "Thu Dec 26 01:14:56 +0000 2013",
+			"favourites_count": 66635,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16416,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1518687457478266880/Q5Nzpu8-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1518687457478266880/Q5Nzpu8-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2262122917/1644516459",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 597,
+		"favorite_count": 8947,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dream Blunt rotation https://t.co/fbl0DRBAQ9"
+	},
+	{
 		"created_at": "Sat Aug 06 12:56:11 +0000 2022",
 		"id": 1555900545117954000,
 		"id_str": "1555900545117954048",
