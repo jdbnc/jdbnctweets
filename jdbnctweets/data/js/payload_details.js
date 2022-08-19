9ae6374e4628f545f9d7a3dee0929c95a80855b0
John Bence <johndbence@gmail.com>
2022-08-18 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3963,
-	"created_at": "Mon Aug 15 03:19:34 +0000 2022",
+	"tweets": 3964,
+	"created_at": "Fri Aug 19 03:19:35 +0000 2022",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2022_08.js
+++ jdbnctweets/data/js/tweets/2022_08.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Thu Aug 18 22:49:19 +0000 2022",
+		"id": 1560397895098159000,
+		"id_str": "1560398466463010817",
+		"full_text": "the drunk ship reeled and shook as if smitten by an iceberg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			59
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://twitter.com/MobyDickatSea\" rel=\"nofollow\">Moby Dick at Sea</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1482779976,
+			"id_str": "1482779976",
+			"name": "Moby Dick",
+			"screen_name": "MobyDickatSea",
+			"location": "The South Seas",
+			"description": "Random lines from Moby Dick. Bot conceived, coded, and curated by Mark Sample. Companion bot @FindMobyDick posts citations.",
+			"url": "https://t.co/ZSPUkwbUo7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZSPUkwbUo7",
+							"expanded_url": "https://ko-fi.com/mobydickatsea",
+							"display_url": "ko-fi.com/mobydickatsea",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 97646,
+			"friends_count": 1,
+			"listed_count": 838,
+			"created_at": "Tue Jun 04 17:23:46 +0000 2013",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 32281,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "3289B5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3755781902/9b47b7e223799bb523c7628e00b411c4_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3755781902/9b47b7e223799bb523c7628e00b411c4_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1482779976/1638113751",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 58,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "the drunk ship reeled and shook as if smitten by an iceberg"
+	},
+	{
 		"created_at": "Mon Aug 15 00:25:49 +0000 2022",
 		"id": 1558629357932884000,
 		"id_str": "1558973199764914176",
@@ -214,7 +310,7 @@ Grailbird.data.tweets_2022_08 =
 			"name": "Christian Tucci",
 			"screen_name": "chrtucci",
 			"location": "Los Angeles, CA",
-			"description": "writer & artist 🏳️‍🌈 he/him 👻👻🔎 $chrtucci drawings are for sale, DM if interested",
+			"description": "writer & artist 🏳️🌈 he/him 👻👻🔎 $chrtucci drawings are for sale, DM if interested",
 			"url": "https://t.co/UzYZzKBEk7",
 			"entities": {
 				"url": {
