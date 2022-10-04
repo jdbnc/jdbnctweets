8a46b37243db7252a8eee84c9084ee632a780939
John Bence <johndbence@gmail.com>
2022-10-03 23:19:36 -0400

    Added 1 tweets

---
+++ jdbnctweets/data/js/tweets/2022_10.js
@@ -0,0 +1,307 @@
+Grailbird.data.tweets_2022_10 = 
+[
+	{
+		"created_at": "Mon Oct 03 18:45:04 +0000 2022",
+		"id": 1577006839199420400,
+		"id_str": "1577006839199420416",
+		"full_text": "Give them a follow - the stories you read may make your blood boil about bad urban planning choices, but you’ll be informed! https://t.co/FRJ4CYvPCg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			124
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/FRJ4CYvPCg",
+					"expanded_url": "https://twitter.com/UrbanizeATL/status/1575965491285282816",
+					"display_url": "twitter.com/UrbanizeATL/st…",
+					"indices": [
+						125,
+						148
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, they let me be an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 734,
+			"friends_count": 1096,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 6980,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3861,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1575965491285282800,
+		"quoted_status_id_str": "1575965491285282816",
+		"quoted_status_permalink": {
+			"url": "https://t.co/FRJ4CYvPCg",
+			"expanded": "https://twitter.com/UrbanizeATL/status/1575965491285282816",
+			"display": "twitter.com/UrbanizeATL/st…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Sep 30 21:47:07 +0000 2022",
+			"id": 1575965491285282800,
+			"id_str": "1575965491285282816",
+			"full_text": "Hey, #Atlanta: We're trying to reach 5K @Twitter followers by early October. (Almost there). If you enjoy @UrbanizeATL coverage, please R/T to help tell your friends. We really appreciate it! 🤗 https://t.co/TGvEFeH4DN",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				193
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Atlanta",
+						"indices": [
+							5,
+							13
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Twitter",
+						"name": "Twitter",
+						"id": 783214,
+						"id_str": "783214",
+						"indices": [
+							40,
+							48
+						]
+					},
+					{
+						"screen_name": "UrbanizeATL",
+						"name": "Urbanize Atlanta",
+						"id": 1356077048758902800,
+						"id_str": "1356077048758902785",
+						"indices": [
+							106,
+							118
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1575964678731219000,
+						"id_str": "1575964678731218963",
+						"indices": [
+							194,
+							217
+						],
+						"media_url": "http://pbs.twimg.com/media/Fd7zhoZWQBMWlK1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Fd7zhoZWQBMWlK1.jpg",
+						"url": "https://t.co/TGvEFeH4DN",
+						"display_url": "pic.twitter.com/TGvEFeH4DN",
+						"expanded_url": "https://twitter.com/UrbanizeATL/status/1575965491285282816/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 971,
+								"h": 731,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 971,
+								"h": 731,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1575964678731219000,
+						"id_str": "1575964678731218963",
+						"indices": [
+							194,
+							217
+						],
+						"media_url": "http://pbs.twimg.com/media/Fd7zhoZWQBMWlK1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Fd7zhoZWQBMWlK1.jpg",
+						"url": "https://t.co/TGvEFeH4DN",
+						"display_url": "pic.twitter.com/TGvEFeH4DN",
+						"expanded_url": "https://twitter.com/UrbanizeATL/status/1575965491285282816/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 971,
+								"h": 731,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 971,
+								"h": 731,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1356077048758902800,
+				"id_str": "1356077048758902785",
+				"name": "Urbanize Atlanta",
+				"screen_name": "UrbanizeATL",
+				"location": "Atlanta",
+				"description": "Covering real estate development, architecture, urban planning, transportation, and other aspects of metro Atlanta's physical evolution.",
+				"url": "https://t.co/LAHcsyl19s",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/LAHcsyl19s",
+								"expanded_url": "https://urbanize.city/atlanta/",
+								"display_url": "urbanize.city/atlanta/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4815,
+				"friends_count": 756,
+				"listed_count": 88,
+				"created_at": "Mon Feb 01 03:09:23 +0000 2021",
+				"favourites_count": 7513,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4018,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1356077199787393027/AAAXSmlt_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1356077199787393027/AAAXSmlt_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1356077048758902785/1612149227",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 31,
+			"favorite_count": 52,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Give them a follow - the stories you read may make your blood boil about bad urban planning choices, but you’ll be informed! https://t.co/FRJ4CYvPCg"
+	}
+]
