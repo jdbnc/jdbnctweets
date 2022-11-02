e20ffb207783fff158f8e1528023cd2c61f1c3ff
John Bence <johndbence@gmail.com>
2022-11-01 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3982,
-	"created_at": "Sun Oct 30 03:19:35 +0000 2022",
+	"tweets": 3983,
+	"created_at": "Wed Nov 02 03:19:35 +0000 2022",
 	"lang": "en"
 }
---
+++ jdbnctweets/data/js/tweets/2022_11.js
@@ -0,0 +1,107 @@
+Grailbird.data.tweets_2022_11 = 
+[
+	{
+		"created_at": "Tue Nov 01 16:10:35 +0000 2022",
+		"id": 1587477210700693500,
+		"id_str": "1587477210700693504",
+		"full_text": "@theearchivist_ @SarahNQuigley Do you like the taste of butter?",
+		"truncated": false,
+		"display_text_range": [
+			31,
+			63
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "theearchivist_",
+					"name": "Meaghan Thee (COZY) Archivist",
+					"id": 23823656,
+					"id_str": "23823656",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "SarahNQuigley",
+					"name": "Dear, dear Sarah.",
+					"id": 2232174620,
+					"id_str": "2232174620",
+					"indices": [
+						16,
+						30
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1587477101438976000,
+		"in_reply_to_status_id_str": "1587477101438976000",
+		"in_reply_to_user_id": 23823656,
+		"in_reply_to_user_id_str": "23823656",
+		"in_reply_to_screen_name": "theearchivist_",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, they let me be an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 724,
+			"friends_count": 1095,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 7046,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3860,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@theearchivist_ @SarahNQuigley Do you like the taste of butter?"
+	}
+]
