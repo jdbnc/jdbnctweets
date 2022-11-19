bed513f6eaa77a8f2acae6badcd9d19af50daa89
John Bence <johndbence@gmail.com>
2022-11-18 22:19:36 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3986,
-	"created_at": "Fri Nov 18 03:19:35 +0000 2022",
+	"tweets": 3987,
+	"created_at": "Sat Nov 19 03:19:35 +0000 2022",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2022_11.js
+++ jdbnctweets/data/js/tweets/2022_11.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Fri Nov 18 19:55:37 +0000 2022",
+		"id": 1593454846744707000,
+		"id_str": "1593694434515984386",
+		"full_text": "https://t.co/aXpa1znrBu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1593454827761275000,
+					"id_str": "1593454827761274880",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/Fh0WuS5XkAA4TaK.png",
+					"media_url_https": "https://pbs.twimg.com/media/Fh0WuS5XkAA4TaK.png",
+					"url": "https://t.co/aXpa1znrBu",
+					"display_url": "pic.twitter.com/aXpa1znrBu",
+					"expanded_url": "https://twitter.com/amandaach/status/1593454846744707074/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 398,
+							"h": 246,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 398,
+							"h": 246,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 398,
+							"h": 246,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1593454827761275000,
+					"id_str": "1593454827761274880",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/Fh0WuS5XkAA4TaK.png",
+					"media_url_https": "https://pbs.twimg.com/media/Fh0WuS5XkAA4TaK.png",
+					"url": "https://t.co/aXpa1znrBu",
+					"display_url": "pic.twitter.com/aXpa1znrBu",
+					"expanded_url": "https://twitter.com/amandaach/status/1593454846744707074/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 398,
+							"h": 246,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 398,
+							"h": 246,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 398,
+							"h": 246,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 604688577,
+			"id_str": "604688577",
+			"name": "Amanda Ach",
+			"screen_name": "amandaach",
+			"location": "",
+			"description": "Big fan of @MajorityPAC. Enemy of birds and comma splices. Tweets are not approved. Previously: digital for @dccc, @RitaHartIA, @TeamHeinrich",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1755,
+			"friends_count": 1767,
+			"listed_count": 18,
+			"created_at": "Sun Jun 10 15:35:38 +0000 2012",
+			"favourites_count": 26984,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5846,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1414212224676159490/Yb4mJgYC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1414212224676159490/Yb4mJgYC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/604688577/1562807684",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 610,
+		"favorite_count": 3365,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "zxx",
+		"text": "https://t.co/aXpa1znrBu"
+	},
+	{
 		"created_at": "Thu Nov 17 17:10:00 +0000 2022",
 		"id": 1593290369248149500,
 		"id_str": "1593290369248149507",
