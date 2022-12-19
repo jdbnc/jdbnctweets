d8cfebfb710ed1b4d3dfb8774e338aa1997b3cd5
John Bence <johndbence@gmail.com>
2022-12-18 22:19:36 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3992,
-	"created_at": "Sat Dec 10 03:19:35 +0000 2022",
+	"tweets": 3993,
+	"created_at": "Mon Dec 19 03:19:35 +0000 2022",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2022_12.js
+++ jdbnctweets/data/js/tweets/2022_12.js
@@ -1,6 +1,280 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Sun Dec 18 19:32:12 +0000 2022",
+		"id": 1604550270331097000,
+		"id_str": "1604560177256177669",
+		"full_text": "https://t.co/kz2WRyJALa https://t.co/PZa5DogXe8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kz2WRyJALa",
+					"expanded_url": "https://twitter.com/GraceOnFootball/status/1604541537538433026",
+					"display_url": "twitter.com/GraceOnFootbal…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1604550266275127300,
+					"id_str": "1604550266275127298",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/FkSB90WWYAIMOTQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FkSB90WWYAIMOTQ.jpg",
+					"url": "https://t.co/PZa5DogXe8",
+					"display_url": "pic.twitter.com/PZa5DogXe8",
+					"expanded_url": "https://twitter.com/Beaumxntt/status/1604550270331097100/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 828,
+							"h": 828,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 828,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1604550266275127300,
+					"id_str": "1604550266275127298",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/FkSB90WWYAIMOTQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FkSB90WWYAIMOTQ.jpg",
+					"url": "https://t.co/PZa5DogXe8",
+					"display_url": "pic.twitter.com/PZa5DogXe8",
+					"expanded_url": "https://twitter.com/Beaumxntt/status/1604550270331097100/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 828,
+							"h": 828,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 828,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3301442693,
+			"id_str": "3301442693",
+			"name": "M 🎅🏼🏴󠁧󠁢󠁥󠁮󠁧󠁿",
+			"screen_name": "Beaumxntt",
+			"location": "Wetherspoons ",
+			"description": "SO14 Mush | H♥️ | #SemmensOut | Have the misfortune of supporting Southampton FC and Lewis Hamilton",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1729,
+			"friends_count": 1896,
+			"listed_count": 7,
+			"created_at": "Thu May 28 08:45:50 +0000 2015",
+			"favourites_count": 38112,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19538,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1468239342841831436/KNTPsbkH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1468239342841831436/KNTPsbkH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3301442693/1671396335",
+			"profile_link_color": "FF0029",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1604541537538433000,
+		"quoted_status_id_str": "1604541537538433026",
+		"quoted_status_permalink": {
+			"url": "https://t.co/kz2WRyJALa",
+			"expanded": "https://twitter.com/GraceOnFootball/status/1604541537538433026",
+			"display": "twitter.com/GraceOnFootbal…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Dec 18 18:18:08 +0000 2022",
+			"id": 1604541537538433000,
+			"id_str": "1604541537538433026",
+			"full_text": "The Premier League starts again in eight days.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				46
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 988555385215823900,
+				"id_str": "988555385215823873",
+				"name": "Grace Robertson 🏳️‍⚧️",
+				"screen_name": "GraceOnFootball",
+				"location": "UK",
+				"description": "Football writer. Newsletter-er. Frequent insomniac. Seen @FiveThirtyEight @Eurosport_UK @StatsBomb @VanityFair etc. Trans, she/her. Graceonfootball at gmail.",
+				"url": "https://t.co/klPqXityJC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/klPqXityJC",
+								"expanded_url": "http://onfootball.substack.com",
+								"display_url": "onfootball.substack.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 52759,
+				"friends_count": 728,
+				"listed_count": 898,
+				"created_at": "Mon Apr 23 23:09:05 +0000 2018",
+				"favourites_count": 53594,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 45953,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1425576358743318534/6-u9CEK-_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1425576358743318534/6-u9CEK-_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/988555385215823873/1628618675",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 46,
+			"favorite_count": 428,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "zxx",
+		"text": "https://t.co/kz2WRyJALa https://t.co/PZa5DogXe8"
+	},
+	{
 		"created_at": "Fri Dec 09 17:52:32 +0000 2022",
 		"id": 1601273604171354000,
 		"id_str": "1601273604171354118",
