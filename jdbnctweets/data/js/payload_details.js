63a7d808af7152b6947fb677a5150d77c72d0c45
John Bence <johndbence@gmail.com>
2023-01-18 22:19:36 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3998,
-	"created_at": "Sat Jan 14 03:19:35 +0000 2023",
+	"tweets": 3999,
+	"created_at": "Thu Jan 19 03:19:35 +0000 2023",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2023_01.js
+++ jdbnctweets/data/js/tweets/2023_01.js
@@ -1,6 +1,328 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Wed Jan 18 16:00:10 +0000 2023",
+		"id": 1615740842173415400,
+		"id_str": "1615740842173415427",
+		"full_text": "Knew it was coming but still hating it 😢 #atlutd https://t.co/Yu1mEjng9W",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			48
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "atlutd",
+					"indices": [
+						41,
+						48
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Yu1mEjng9W",
+					"expanded_url": "https://twitter.com/ATLUTD/status/1615719551563358210",
+					"display_url": "twitter.com/ATLUTD/status/…",
+					"indices": [
+						49,
+						72
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, they let me be an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 707,
+			"friends_count": 1074,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 7149,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3864,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1615719551563358200,
+		"quoted_status_id_str": "1615719551563358210",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Yu1mEjng9W",
+			"expanded": "https://twitter.com/ATLUTD/status/1615719551563358210",
+			"display": "twitter.com/ATLUTD/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jan 18 14:35:34 +0000 2023",
+			"id": 1615719551563358200,
+			"id_str": "1615719551563358210",
+			"full_text": "An icon. A legend. Forever a 5-Stripe.\n\nYou loved Atlanta and Atlanta loved you right back. Thank you for all the memories, Josef ❤️🖤 https://t.co/70rYoqcKkt",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				133
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1615549001780142000,
+						"id_str": "1615549001780142082",
+						"indices": [
+							134,
+							157
+						],
+						"media_url": "http://pbs.twimg.com/media/FmuxQ8AX0AEi7yX.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FmuxQ8AX0AEi7yX.jpg",
+						"url": "https://t.co/70rYoqcKkt",
+						"display_url": "pic.twitter.com/70rYoqcKkt",
+						"expanded_url": "https://twitter.com/ATLUTD/status/1615719551563358210/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1615549001780142000,
+						"id_str": "1615549001780142082",
+						"indices": [
+							134,
+							157
+						],
+						"media_url": "http://pbs.twimg.com/media/FmuxQ8AX0AEi7yX.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FmuxQ8AX0AEi7yX.jpg",
+						"url": "https://t.co/70rYoqcKkt",
+						"display_url": "pic.twitter.com/70rYoqcKkt",
+						"expanded_url": "https://twitter.com/ATLUTD/status/1615719551563358210/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 128042,
+							"variants": [
+								{
+									"bitrate": 288000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1615549001780142082/vid/480x270/dJCoNWKDeKeQaFbw.mp4?tag=16"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/amplify_video/1615549001780142082/pl/V_FYez_fkqBYE7yc.m3u8?tag=16&container=fmp4"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1615549001780142082/vid/1280x720/nNk4dX5J9YX4UJ8F.mp4?tag=16"
+								},
+								{
+									"bitrate": 10368000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1615549001780142082/vid/1920x1080/5U2R8CTcdBCexB02.mp4?tag=16"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1615549001780142082/vid/640x360/sip-hmJ9latoSB4t.mp4?tag=16"
+								}
+							]
+						},
+						"additional_media_info": {
+							"title": "",
+							"description": "",
+							"embeddable": true,
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2413176055,
+				"id_str": "2413176055",
+				"name": "Atlanta United FC",
+				"screen_name": "ATLUTD",
+				"location": "Atlanta, GA",
+				"description": "𝟑𝟗 𝐃𝐀𝐘𝐒 𝐔𝐍𝐓𝐈𝐋 𝐓𝐇𝐄 𝟐𝟎𝟐𝟑 𝐇𝐎𝐌𝐄 𝐎𝐏𝐄𝐍𝐄𝐑  | En español: @VamosATLUTD 🏆🏆🏆🏆",
+				"url": "https://t.co/plJXKoNRzp",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/plJXKoNRzp",
+								"expanded_url": "http://atlutd.com/tickets",
+								"display_url": "atlutd.com/tickets",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 999630,
+				"friends_count": 357,
+				"listed_count": 1811,
+				"created_at": "Wed Mar 26 20:49:38 +0000 2014",
+				"favourites_count": 47492,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 36764,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1576631355198283776/LTCFFEkt_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1576631355198283776/LTCFFEkt_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2413176055/1668796269",
+				"profile_link_color": "80000B",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 560,
+			"favorite_count": 2288,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Knew it was coming but still hating it 😢 #atlutd https://t.co/Yu1mEjng9W"
+	},
+	{
 		"created_at": "Fri Jan 13 03:45:08 +0000 2023",
 		"id": 1613743926413824000,
 		"id_str": "1613743926413824000",
