973032ac1b70bc6d790ae4a514143547c21e050e
John Bence <johndbence@gmail.com>
2023-01-20 22:19:41 -0500

    Added 2 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4000,
-	"created_at": "Fri Jan 20 03:19:35 +0000 2023",
+	"tweets": 4002,
+	"created_at": "Sat Jan 21 03:19:39 +0000 2023",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2023_01.js
+++ jdbnctweets/data/js/tweets/2023_01.js
@@ -1,6 +1,415 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Fri Jan 20 21:28:31 +0000 2023",
+		"id": 1616548250701516800,
+		"id_str": "1616548250701516806",
+		"full_text": "@mattn1827 @SouthamptonFC @FrannyBenali @PLinUSA Wish I had planned better to be there this weekend! Looks awesome.",
+		"truncated": false,
+		"display_text_range": [
+			49,
+			115
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mattn1827",
+					"name": "Matt",
+					"id": 314763233,
+					"id_str": "314763233",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "SouthamptonFC",
+					"name": "Southampton FC",
+					"id": 46143892,
+					"id_str": "46143892",
+					"indices": [
+						11,
+						25
+					]
+				},
+				{
+					"screen_name": "FrannyBenali",
+					"name": "Francis Benali MBE",
+					"id": 2257808186,
+					"id_str": "2257808186",
+					"indices": [
+						26,
+						39
+					]
+				},
+				{
+					"screen_name": "PLinUSA",
+					"name": "Premier League USA",
+					"id": 822225946916884500,
+					"id_str": "822225946916884480",
+					"indices": [
+						40,
+						48
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1616499251009273900,
+		"in_reply_to_status_id_str": "1616499251009273863",
+		"in_reply_to_user_id": 314763233,
+		"in_reply_to_user_id_str": "314763233",
+		"in_reply_to_screen_name": "mattn1827",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, they let me be an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 708,
+			"friends_count": 1075,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 7160,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3868,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@mattn1827 @SouthamptonFC @FrannyBenali @PLinUSA Wish I had planned better to be there this weekend! Looks awesome."
+	},
+	{
+		"created_at": "Fri Jan 20 19:49:41 +0000 2023",
+		"id": 1616523379892260900,
+		"id_str": "1616523379892260866",
+		"full_text": "😞 https://t.co/16sYmDg617",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			1
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/16sYmDg617",
+					"expanded_url": "https://twitter.com/tapbots/status/1616504262971822106",
+					"display_url": "twitter.com/tapbots/status…",
+					"indices": [
+						2,
+						25
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, they let me be an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 708,
+			"friends_count": 1075,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 7160,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3868,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1616504262971822000,
+		"quoted_status_id_str": "1616504262971822106",
+		"quoted_status_permalink": {
+			"url": "https://t.co/16sYmDg617",
+			"expanded": "https://twitter.com/tapbots/status/1616504262971822106",
+			"display": "twitter.com/tapbots/status…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jan 20 18:33:44 +0000 2023",
+			"id": 1616504262971822000,
+			"id_str": "1616504262971822106",
+			"full_text": "Say Goodbye to Tweetbot.\n\nWe've been proud to serve you over the last 12+ years, but due to circumstances beyond our control, we have to shut down Tweetbot.\n\nThank you so much for your patience and outpouring of support over these tough times at Tapbots. \n\nhttps://t.co/PjHePIkCpb https://t.co/e45XXU7ugF",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/PjHePIkCpb",
+						"expanded_url": "https://tapbots.com/tweetbot/",
+						"display_url": "tapbots.com/tweetbot/",
+						"indices": [
+							257,
+							280
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1616497333969080300,
+						"id_str": "1616497333969080320",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/Fm7zwruX0AAMFXa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Fm7zwruX0AAMFXa.jpg",
+						"url": "https://t.co/e45XXU7ugF",
+						"display_url": "pic.twitter.com/e45XXU7ugF",
+						"expanded_url": "https://twitter.com/tapbots/status/1616504262971822106/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1365,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1616497333969080300,
+						"id_str": "1616497333969080320",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/Fm7zwruX0AAMFXa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Fm7zwruX0AAMFXa.jpg",
+						"url": "https://t.co/e45XXU7ugF",
+						"display_url": "pic.twitter.com/e45XXU7ugF",
+						"expanded_url": "https://twitter.com/tapbots/status/1616504262971822106/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1365,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16669898,
+				"id_str": "16669898",
+				"name": "Tapbots",
+				"screen_name": "tapbots",
+				"location": "",
+				"description": "We create delightful apps for iOS and the Mac.",
+				"url": "https://t.co/FoDykOr4kI",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/FoDykOr4kI",
+								"expanded_url": "http://tapbots.com",
+								"display_url": "tapbots.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 288862,
+				"friends_count": 4,
+				"listed_count": 5542,
+				"created_at": "Thu Oct 09 17:47:49 +0000 2008",
+				"favourites_count": 0,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 15747,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1590743889601765376/UYZPExYX_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1590743889601765376/UYZPExYX_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16669898/1668097873",
+				"profile_link_color": "CC0000",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "F3F3F3",
+				"profile_text_color": "2E2E2E",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2792,
+			"favorite_count": 8796,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "art",
+		"text": "😞 https://t.co/16sYmDg617"
+	},
+	{
 		"created_at": "Thu Jan 19 14:27:59 +0000 2023",
 		"id": 1616080030765056000,
 		"id_str": "1616080030765056003",
