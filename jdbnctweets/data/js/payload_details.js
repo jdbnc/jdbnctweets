965da983ef18c93b0d5e178525169c8b0f1399ac
John Bence <johndbence@gmail.com>
2023-02-18 22:19:36 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4007,
-	"created_at": "Wed Feb 15 03:19:34 +0000 2023",
+	"tweets": 4008,
+	"created_at": "Sun Feb 19 03:19:35 +0000 2023",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2023_02.js
+++ jdbnctweets/data/js/tweets/2023_02.js
@@ -1,6 +1,328 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Sat Feb 18 20:47:00 +0000 2023",
+		"id": 1627047050205618200,
+		"id_str": "1627047050205618176",
+		"full_text": "Selles szn #saintsfc https://t.co/w8tZhkCaz7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "saintsfc",
+					"indices": [
+						11,
+						20
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/w8tZhkCaz7",
+					"expanded_url": "https://twitter.com/j_tanswell/status/1626991099456757760",
+					"display_url": "twitter.com/j_tanswell/sta…",
+					"indices": [
+						21,
+						44
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, they let me be an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 711,
+			"friends_count": 1079,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 7270,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3873,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1626991099456757800,
+		"quoted_status_id_str": "1626991099456757760",
+		"quoted_status_permalink": {
+			"url": "https://t.co/w8tZhkCaz7",
+			"expanded": "https://twitter.com/j_tanswell/status/1626991099456757760",
+			"display": "twitter.com/j_tanswell/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Feb 18 17:04:40 +0000 2023",
+			"id": 1626991099456757800,
+			"id_str": "1626991099456757760",
+			"full_text": "Full-time scenes. Incredible. \n\n#SaintsFC https://t.co/4KGcm5Z5mh",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				41
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "SaintsFC",
+						"indices": [
+							32,
+							41
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1626991032427487200,
+						"id_str": "1626991032427487233",
+						"indices": [
+							42,
+							65
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1626991032427487233/pu/img/lKgVmHCoeyKYUQq5.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1626991032427487233/pu/img/lKgVmHCoeyKYUQq5.jpg",
+						"url": "https://t.co/4KGcm5Z5mh",
+						"display_url": "pic.twitter.com/4KGcm5Z5mh",
+						"expanded_url": "https://twitter.com/J_Tanswell/status/1626991099456757760/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1626991032427487200,
+						"id_str": "1626991032427487233",
+						"indices": [
+							42,
+							65
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1626991032427487233/pu/img/lKgVmHCoeyKYUQq5.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1626991032427487233/pu/img/lKgVmHCoeyKYUQq5.jpg",
+						"url": "https://t.co/4KGcm5Z5mh",
+						"display_url": "pic.twitter.com/4KGcm5Z5mh",
+						"expanded_url": "https://twitter.com/J_Tanswell/status/1626991099456757760/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 9700,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1626991032427487233/pu/pl/IWLE0hV1-u6xHf6y.m3u8?tag=12&container=fmp4"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1626991032427487233/pu/vid/480x270/9AhH0qH2CowD5z4j.mp4?tag=12"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1626991032427487233/pu/vid/640x360/MowCgmIAr5_1WYxm.mp4?tag=12"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1626991032427487233/pu/vid/1280x720/2k_L1-rTXRwvXXRe.mp4?tag=12"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 978981349234217000,
+				"id_str": "978981349234216966",
+				"name": "Jacob Tanswell",
+				"screen_name": "J_Tanswell",
+				"location": "Hampshire, England",
+				"description": "Southampton FC reporter for @TheAthleticFC |#SaintsFC|@theofficialfwa's Hugh McIlvanney Student Football Writer of the Year 2021.\nE:jtanswell@theathletic.com",
+				"url": "https://t.co/fLMZsK291p",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/fLMZsK291p",
+								"expanded_url": "https://theathletic.com/author/jacob-tanswell/",
+								"display_url": "theathletic.com/author/jacob-t…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 15216,
+				"friends_count": 628,
+				"listed_count": 112,
+				"created_at": "Wed Mar 28 13:05:17 +0000 2018",
+				"favourites_count": 6110,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 10886,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1538811706163486721/p92WVl6t_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1538811706163486721/p92WVl6t_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/978981349234216966/1638884637",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 85,
+			"favorite_count": 788,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "hu",
+		"text": "Selles szn #saintsfc https://t.co/w8tZhkCaz7"
+	},
+	{
 		"created_at": "Tue Feb 14 18:06:28 +0000 2023",
 		"id": 1625557100423221200,
 		"id_str": "1625557100423221250",
