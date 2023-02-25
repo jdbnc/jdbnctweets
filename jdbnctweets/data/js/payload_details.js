ff8997a16e96b4fb8cfe6d8409318a4969f2f452
John Bence <johndbence@gmail.com>
2023-02-24 22:19:35 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4010,
-	"created_at": "Fri Feb 24 03:19:34 +0000 2023",
+	"tweets": 4011,
+	"created_at": "Sat Feb 25 03:19:34 +0000 2023",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2023_02.js
+++ jdbnctweets/data/js/tweets/2023_02.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Fri Feb 24 16:31:55 +0000 2023",
+		"id": 1629157184427724800,
+		"id_str": "1629157184427724801",
+		"full_text": "@paysoninho My thoughts exactly. Also, makes a Philly match sound like a dour non-party.",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			88
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "paysoninho",
+					"name": "Paysoninho",
+					"id": 15098000,
+					"id_str": "15098000",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1629156891296206800,
+		"in_reply_to_status_id_str": "1629156891296206848",
+		"in_reply_to_user_id": 15098000,
+		"in_reply_to_user_id_str": "15098000",
+		"in_reply_to_screen_name": "paysoninho",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, they let me be an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 711,
+			"friends_count": 1082,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 7289,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3875,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@paysoninho My thoughts exactly. Also, makes a Philly match sound like a dour non-party."
+	},
+	{
 		"created_at": "Thu Feb 23 16:23:54 +0000 2023",
 		"id": 1628792778607009800,
 		"id_str": "1628792778607009793",
