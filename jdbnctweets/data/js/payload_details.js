f78250e9ecb333354ac4a7874122a93b01f2da16
John Bence <johndbence@gmail.com>
2023-03-02 22:19:36 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4011,
-	"created_at": "Sat Feb 25 03:19:34 +0000 2023",
+	"tweets": 4012,
+	"created_at": "Fri Mar 03 03:19:35 +0000 2023",
 	"lang": "en"
 }
---
+++ jdbnctweets/data/js/tweets/2023_03.js
@@ -0,0 +1,128 @@
+Grailbird.data.tweets_2023_03 = 
+[
+	{
+		"created_at": "Thu Mar 02 16:45:30 +0000 2023",
+		"id": 1631334929366753300,
+		"id_str": "1631334929366753305",
+		"full_text": "@DrKarenLCox @AHAhistorians @The_OAH Archivists are talking about it: https://t.co/0BVltpyrb6",
+		"truncated": false,
+		"display_text_range": [
+			37,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DrKarenLCox",
+					"name": "@DrKarenLCox 🏳️‍🌈",
+					"id": 294500046,
+					"id_str": "294500046",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "AHAhistorians",
+					"name": "American Historical Association",
+					"id": 76944176,
+					"id_str": "76944176",
+					"indices": [
+						13,
+						27
+					]
+				},
+				{
+					"screen_name": "The_OAH",
+					"name": "Organization of American Historians",
+					"id": 58882425,
+					"id_str": "58882425",
+					"indices": [
+						28,
+						36
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/0BVltpyrb6",
+					"expanded_url": "https://elischolar.library.yale.edu/cgi/viewcontent.cgi?article=1090&context=jcas",
+					"display_url": "elischolar.library.yale.edu/cgi/viewconten…",
+					"indices": [
+						70,
+						93
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1631330960523370500,
+		"in_reply_to_status_id_str": "1631330960523370499",
+		"in_reply_to_user_id": 294500046,
+		"in_reply_to_user_id_str": "294500046",
+		"in_reply_to_screen_name": "DrKarenLCox",
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, they let me be an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 710,
+			"friends_count": 1079,
+			"listed_count": 28,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 7306,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3876,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@DrKarenLCox @AHAhistorians @The_OAH Archivists are talking about it: https://t.co/0BVltpyrb6"
+	}
+]
