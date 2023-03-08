84a5a4320f7dc680bf20dea962c5cc6e265c22dd
John Bence <johndbence@gmail.com>
2023-03-07 22:19:36 -0500

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4012,
-	"created_at": "Fri Mar 03 03:19:35 +0000 2023",
+	"tweets": 4013,
+	"created_at": "Wed Mar 08 03:19:35 +0000 2023",
 	"lang": "en"
 }
--- jdbnctweets/data/js/tweets/2023_03.js
+++ jdbnctweets/data/js/tweets/2023_03.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Tue Mar 07 19:20:24 +0000 2023",
+		"id": 1633185266684018700,
+		"id_str": "1633185848660475904",
+		"full_text": "Atlanta, GA: Head of Archival Processing, Emory University https://t.co/20QLeQEzvA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			82
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/20QLeQEzvA",
+					"expanded_url": "http://archivesgig.com/2023/03/07/atlanta-ga-head-of-archival-processing-emory-university/",
+					"display_url": "archivesgig.com/2023/03/07/atl…",
+					"indices": [
+						59,
+						82
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">Jetpack.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 142336259,
+			"id_str": "142336259",
+			"name": "Archives Gig",
+			"screen_name": "archivesgig",
+			"location": "Madison, Wisconsin",
+			"description": "Archives, records management, and similarly-flavored jobs and internships, administered by Meredith Lowe (she/her).  Personal Twitter account @meredithclowe",
+			"url": "https://t.co/ejOK3EQ3Wk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ejOK3EQ3Wk",
+							"expanded_url": "http://archivesgig.com",
+							"display_url": "archivesgig.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6190,
+			"friends_count": 3120,
+			"listed_count": 165,
+			"created_at": "Mon May 10 16:19:53 +0000 2010",
+			"favourites_count": 298,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 18555,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/530788285505757184/IRMIcUFV_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/530788285505757184/IRMIcUFV_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/142336259/1480959578",
+			"profile_link_color": "4A913C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Atlanta, GA: Head of Archival Processing, Emory University https://t.co/20QLeQEzvA"
+	},
+	{
 		"created_at": "Thu Mar 02 16:45:30 +0000 2023",
 		"id": 1631334929366753300,
 		"id_str": "1631334929366753305",
@@ -16,7 +123,7 @@ Grailbird.data.tweets_2023_03 =
 			"user_mentions": [
 				{
 					"screen_name": "DrKarenLCox",
-					"name": "@DrKarenLCox 🏳️‍🌈",
+					"name": "@DrKarenLCox 🏳️🌈",
 					"id": 294500046,
 					"id_str": "294500046",
 					"indices": [
