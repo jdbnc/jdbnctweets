3702e92a6e6b3da28e1aa234d47d5bca3a36c250
John Bence <johndbence@gmail.com>
2023-05-13 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/payload_details.js
+++ jdbnctweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4020,
-	"created_at": "Thu Apr 27 03:19:35 +0000 2023",
+	"tweets": 4021,
+	"created_at": "Sun May 14 03:19:35 +0000 2023",
 	"lang": "en"
 }
---
+++ jdbnctweets/data/js/tweets/2023_05.js
@@ -0,0 +1,385 @@
+Grailbird.data.tweets_2023_05 = 
+[
+	{
+		"created_at": "Sat May 13 14:03:30 +0000 2023",
+		"id": 1657386085687734300,
+		"id_str": "1657386085687734272",
+		"full_text": "Up the Saints! #saintsfc https://t.co/aShNDGe1pQ https://t.co/PKlwfYdjoG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			48
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "saintsfc",
+					"indices": [
+						15,
+						24
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/aShNDGe1pQ",
+					"expanded_url": "https://twitter.com/totalsaintspod/status/1657385165033701378",
+					"display_url": "twitter.com/totalsaintspod…",
+					"indices": [
+						25,
+						48
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1657386078465060900,
+					"id_str": "1657386078465060870",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FwA3226WcAY1qn4.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FwA3226WcAY1qn4.jpg",
+					"url": "https://t.co/PKlwfYdjoG",
+					"display_url": "pic.twitter.com/PKlwfYdjoG",
+					"expanded_url": "https://twitter.com/jdbence/status/1657386085687734272/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 220,
+							"h": 220,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 220,
+							"h": 220,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 220,
+							"h": 220,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1657386078465060900,
+					"id_str": "1657386078465060870",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FwA3226WcAY1qn4.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FwA3226WcAY1qn4.jpg",
+					"url": "https://t.co/PKlwfYdjoG",
+					"display_url": "pic.twitter.com/PKlwfYdjoG",
+					"expanded_url": "https://twitter.com/jdbence/status/1657386085687734272/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 220,
+							"h": 220,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 220,
+							"h": 220,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 220,
+							"h": 220,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FwA3226WcAY1qn4.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, they let me be an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 705,
+			"friends_count": 1086,
+			"listed_count": 27,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 7427,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3856,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1657385165033701400,
+		"quoted_status_id_str": "1657385165033701378",
+		"quoted_status_permalink": {
+			"url": "https://t.co/aShNDGe1pQ",
+			"expanded": "https://twitter.com/totalsaintspod/status/1657385165033701378",
+			"display": "twitter.com/totalsaintspod…"
+		},
+		"quoted_status": {
+			"created_at": "Sat May 13 13:59:50 +0000 2023",
+			"id": 1657385165033701400,
+			"id_str": "1657385165033701378",
+			"full_text": "Let’s get relegated! #SaintsFC https://t.co/03kvrxhE5Z",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				30
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "SaintsFC",
+						"indices": [
+							21,
+							30
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1657384934607057000,
+						"id_str": "1657384934607056897",
+						"indices": [
+							31,
+							54
+						],
+						"media_url": "http://pbs.twimg.com/media/FwA20RtWcAEeIHt.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FwA20RtWcAEeIHt.jpg",
+						"url": "https://t.co/03kvrxhE5Z",
+						"display_url": "pic.twitter.com/03kvrxhE5Z",
+						"expanded_url": "https://twitter.com/TotalSaintsPod/status/1657385165033701378/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1657384934607057000,
+						"id_str": "1657384934607056897",
+						"indices": [
+							31,
+							54
+						],
+						"media_url": "http://pbs.twimg.com/media/FwA20RtWcAEeIHt.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FwA20RtWcAEeIHt.jpg",
+						"url": "https://t.co/03kvrxhE5Z",
+						"display_url": "pic.twitter.com/03kvrxhE5Z",
+						"expanded_url": "https://twitter.com/TotalSaintsPod/status/1657385165033701378/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 893695434799153200,
+				"id_str": "893695434799153156",
+				"name": "Total Saints Podcast",
+				"screen_name": "TotalSaintsPod",
+				"location": "Southampton, England",
+				"description": "Est.2017; Dedicated #SaintsFC podcast going to the heart of all things @SouthamptonFC; Underpinned by our amazing TSP Patrons; @talkSPORT Fan Network member; 😇",
+				"url": "https://t.co/HXAo6PRnSu",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/HXAo6PRnSu",
+								"expanded_url": "http://www.totalsaints.co.uk",
+								"display_url": "totalsaints.co.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5980,
+				"friends_count": 4243,
+				"listed_count": 46,
+				"created_at": "Sat Aug 05 04:49:30 +0000 2017",
+				"favourites_count": 7853,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 6298,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1553744588883595267/iLK27L_T_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1553744588883595267/iLK27L_T_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/893695434799153156/1659276552",
+				"profile_link_color": "E81C4F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 90,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Up the Saints! #saintsfc https://t.co/aShNDGe1pQ https://t.co/PKlwfYdjoG"
+	}
+]
