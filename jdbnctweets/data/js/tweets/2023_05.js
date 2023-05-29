bd56bcc10eda9772fd9fc32697db82e8224c68e7
John Bence <johndbence@gmail.com>
2023-05-28 23:19:36 -0400

    Added 1 tweets

--- jdbnctweets/data/js/tweets/2023_05.js
+++ jdbnctweets/data/js/tweets/2023_05.js
@@ -1,6 +1,289 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Sun May 28 18:01:59 +0000 2023",
+		"id": 1662881922995966000,
+		"id_str": "1662881922995965952",
+		"full_text": "I’m not crying you’re crying https://t.co/2imxAWjEAY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			28
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2imxAWjEAY",
+					"expanded_url": "https://twitter.com/totalsaintspod/status/1662875056781893633",
+					"display_url": "twitter.com/totalsaintspod…",
+					"indices": [
+						29,
+						52
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24188638,
+			"id_str": "24188638",
+			"name": "John Bence",
+			"screen_name": "jdbence",
+			"location": "Atlanta",
+			"description": "On good days, they let me be an archivist   •   #ATLUTD #SaintsFC #s04   •",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 712,
+			"friends_count": 1096,
+			"listed_count": 27,
+			"created_at": "Fri Mar 13 15:25:29 +0000 2009",
+			"favourites_count": 7529,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3865,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1432855711520862215/zkApzUC2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24188638/1596533389",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "12B8A5",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1662875056781893600,
+		"quoted_status_id_str": "1662875056781893633",
+		"quoted_status_permalink": {
+			"url": "https://t.co/2imxAWjEAY",
+			"expanded": "https://twitter.com/totalsaintspod/status/1662875056781893633",
+			"display": "twitter.com/totalsaintspod…"
+		},
+		"quoted_status": {
+			"created_at": "Sun May 28 17:34:42 +0000 2023",
+			"id": 1662875056781893600,
+			"id_str": "1662875056781893633",
+			"full_text": "Thank you ‘Mr Southampton’. 🥹 #SaintsFC https://t.co/LDcGMHiBaz",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				39
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "SaintsFC",
+						"indices": [
+							30,
+							39
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1662875052939915300,
+						"id_str": "1662875052939915269",
+						"indices": [
+							40,
+							63
+						],
+						"media_url": "http://pbs.twimg.com/media/FxO4DORXsAU3QdY.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FxO4DORXsAU3QdY.jpg",
+						"url": "https://t.co/LDcGMHiBaz",
+						"display_url": "pic.twitter.com/LDcGMHiBaz",
+						"expanded_url": "https://twitter.com/TotalSaintsPod/status/1662875056781893633/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1662875052939915300,
+						"id_str": "1662875052939915269",
+						"indices": [
+							40,
+							63
+						],
+						"media_url": "http://pbs.twimg.com/media/FxO4DORXsAU3QdY.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FxO4DORXsAU3QdY.jpg",
+						"url": "https://t.co/LDcGMHiBaz",
+						"display_url": "pic.twitter.com/LDcGMHiBaz",
+						"expanded_url": "https://twitter.com/TotalSaintsPod/status/1662875056781893633/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 893695434799153200,
+				"id_str": "893695434799153156",
+				"name": "Total Saints Podcast",
+				"screen_name": "TotalSaintsPod",
+				"location": "Southampton, England",
+				"description": "Est.2017; Dedicated #SaintsFC podcast going to the heart of all things @SouthamptonFC; Underpinned by our amazing TSP Patrons; @talkSPORT Fan Network member; 😇",
+				"url": "https://t.co/HXAo6PRnSu",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/HXAo6PRnSu",
+								"expanded_url": "http://www.totalsaints.co.uk",
+								"display_url": "totalsaints.co.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6070,
+				"friends_count": 4228,
+				"listed_count": 46,
+				"created_at": "Sat Aug 05 04:49:30 +0000 2017",
+				"favourites_count": 7988,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 6365,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1553744588883595267/iLK27L_T_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1553744588883595267/iLK27L_T_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/893695434799153156/1659276552",
+				"profile_link_color": "E81C4F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 32,
+			"favorite_count": 502,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I’m not crying you’re crying https://t.co/2imxAWjEAY"
+	},
+	{
 		"created_at": "Sat May 27 18:01:28 +0000 2023",
 		"id": 1662519405232763000,
 		"id_str": "1662519405232762883",
